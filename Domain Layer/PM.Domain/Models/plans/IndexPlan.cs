30e7c28bd1bf5fddb05588732f32bdc9cd611b57
Akai <nguyensieu12112002@gmail.com>
2025-02-05 15:59:38 +0700

    Create IndexPlan.cs

---
+++ Domain Layer/PM.Domain/Models/plans/IndexPlan.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Domain.Models.plans
+{
+    public class IndexPlan
+    {
+        public string PlanId { get; set; } = string.Empty;
+        public string PlanName { get; set; } = string.Empty ;
+        public string Status { get; set; } = string.Empty;
+    }
+}
