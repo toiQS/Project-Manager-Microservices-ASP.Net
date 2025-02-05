3a3bea385438f989ca14c0dd0df53c29f8963112
Akai <nguyensieu12112002@gmail.com>
2025-02-05 15:58:39 +0700

    Create IndexPosition.cs

---
+++ Domain Layer/PM.Domain/Models/positions/IndexPosition.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Domain.Models.positions
+{
+    public class IndexPosition
+    {
+        public string PositionId { get; set; } = string.Empty;
+        public string PositionName { get; set; } = string.Empty;
+        public string PrositionDescription { get; set; } = string.Empty;
+    }
+}
