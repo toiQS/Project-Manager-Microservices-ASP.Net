ab2dae0d491d7c98047b74330ed5ed65f8a0f8d2
Akai <nguyensieu12112002@gmail.com>
2025-02-05 15:59:18 +0700

    Create AppUserClaim.cs

---
+++ Domain Layer/PM.Domain/Models/auths/AppUserClaim.cs
@@ -0,0 +1,8 @@
+﻿namespace PM.Domain.Models.auths
+{
+    public class AppUserClaim
+    {
+        public string Email { get; set; }  = string.Empty;
+        public string RoleUser { get; set; } = string.Empty;
+    }
+}
