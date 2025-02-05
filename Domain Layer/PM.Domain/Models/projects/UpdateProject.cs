7cb11ca61d5b594aa3d7ce6374b6414d4333e497
Akai <nguyensieu12112002@gmail.com>
2025-02-05 15:59:00 +0700

    Create UpdateProject.cs

---
+++ Domain Layer/PM.Domain/Models/projects/UpdateProject.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Domain.Models.projects
+{
+    public class UpdateProject
+    {
+        public string ProjectName { get; set; } = string.Empty;
+        public string ProjectDescription { get; set; } = string.Empty;
+       
+    }
+}
