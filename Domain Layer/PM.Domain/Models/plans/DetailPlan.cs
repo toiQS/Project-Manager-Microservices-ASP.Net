59fd908e420d6bb0e7b72971854468508626d298
Akai <nguyensieu12112002@gmail.com>
2025-02-05 15:59:36 +0700

    Create DetailPlan.cs

---
+++ Domain Layer/PM.Domain/Models/plans/DetailPlan.cs
@@ -0,0 +1,13 @@
+﻿using PM.Domain.Models.tasks;
+
+namespace PM.Domain.Models.plans
+{
+    public class DetailPlan
+    {
+        public string PlanId { get; set; } = string.Empty;
+        public string PlanName { get; set; } = string.Empty;
+        public string Status { get; set; } = string.Empty;
+        public bool IsDone { get; set; }
+        public List<IndexTask> Tasks { get; set; } = new List<IndexTask>();
+    }
+}
