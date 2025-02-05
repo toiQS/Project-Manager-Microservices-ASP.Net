d9a354de0b39ca16318d24a8a84529939f507801
Akai <nguyensieu12112002@gmail.com>
2025-02-05 15:59:40 +0700

    Create UpdatePlan.cs

---
+++ Domain Layer/PM.Domain/Models/plans/UpdatePlan.cs
@@ -0,0 +1,8 @@
+﻿namespace PM.Domain.Models.plans
+{
+    public class UpdatePlan
+    {
+        public string PlanName { get; set; } = string.Empty;
+        public string PlanDescription { get; set; } = string.Empty;
+    }
+}
