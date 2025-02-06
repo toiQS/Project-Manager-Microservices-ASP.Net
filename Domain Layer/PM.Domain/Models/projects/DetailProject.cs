ce3e6160d5ec7e0234eaefdb5d18b4650b43bbe7
Akai <nguyensieu12112002@gmail.com>
2025-02-06 21:34:14 +0700

    Update DetailProject.cs

--- Domain Layer/PM.Domain/Models/projects/DetailProject.cs
+++ Domain Layer/PM.Domain/Models/projects/DetailProject.cs
@@ -7,12 +7,11 @@ namespace PM.Domain.Models.projects
     {
         public string ProjectId { get; set; } = string.Empty;
         public string ProjectName { get; set; } = string.Empty;
-        public DateTime StartAt { get; set; }
-        public DateTime EndAt { get; set; }
+        public DateTime? StartAt { get; set; }
+        public DateTime? EndAt { get; set; }
         public DateTime CreateAt { get; set; }
-        public bool IsDeleted { get; set; }
-        public bool IsAccessed { get; set; }
-        public bool IsDone { get; set; }
+        public bool IsDeleted { get; set; } // Đã xóa chưa
+        public bool IsCompleted { get; set; } // Đã hoàn thành chưa
         public string OwnerName { get; set; } = string.Empty; //user name 
         public string OwnerAvata { get; set; } = string.Empty;
         public List<IndexPlan> Plans { get; set; } = new List<IndexPlan>();
