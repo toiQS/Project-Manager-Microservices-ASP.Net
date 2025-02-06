8897af861d693b1fc63efcb8548d461045ba3a6c
Akai <nguyensieu12112002@gmail.com>
2025-02-06 23:06:40 +0700

    Update IndexPlan.cs

--- Domain Layer/PM.Domain/Models/plans/IndexPlan.cs
+++ Domain Layer/PM.Domain/Models/plans/IndexPlan.cs
@@ -4,6 +4,6 @@
     {
         public string PlanId { get; set; } = string.Empty;
         public string PlanName { get; set; } = string.Empty ;
-        public string Status { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
     }
 }
