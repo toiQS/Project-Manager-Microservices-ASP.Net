b2907d4a073e01fceaf9a9c6cf310b882c53d87e
Akai <nguyensieu12112002@gmail.com>
2025-02-06 21:34:08 +0700

    Update AddProject.cs

--- Domain Layer/PM.Domain/Models/projects/AddProject.cs
+++ Domain Layer/PM.Domain/Models/projects/AddProject.cs
@@ -3,8 +3,8 @@
     public class AddProject
     {
         public string ProjectName { get; set; } = string.Empty;
-        public DateTime StartAt { get; set; } = DateTime.Now;
-        public DateTime EndAt { get; set; } = DateTime.Now.AddYears(1);
+        public DateTime StartAt { get; set; } 
+        public DateTime EndAt { get; set; } 
         public string ProjectDescription { get; set; } = string.Empty;
     }
 }
