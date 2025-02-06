c2f0aa115f6ccf33d2e62429e556dd34e7a881f9
Akai <nguyensieu12112002@gmail.com>
2025-02-06 21:34:03 +0700

    Update IProjectServices.cs

--- Domain Layer/PM.Domain/Interfaces/Services/IProjectServices.cs
+++ Domain Layer/PM.Domain/Interfaces/Services/IProjectServices.cs
@@ -1,6 +1,18 @@
-﻿namespace PM.Domain.Interfaces.Services
+﻿using PM.Domain.Models.projects;
+
+namespace PM.Domain.Interfaces.Services
 {
     public interface IProjectServices
     {
+        Task<ServicesResult<IEnumerable<IndexProject>>> GetProductListUserHasJoined(string userId);
+        Task<ServicesResult<IEnumerable<IndexProject>>> GetProjects();
+        Task<ServicesResult<IEnumerable<IndexProject>>> GetProductListUserHasOwner(string userId);
+        Task<ServicesResult<bool>> Add(string userId, AddProject addProject);
+        Task<ServicesResult<bool>> UpdateInfo(string userId, string projectId, UpdateProject updateProject);
+        Task<ServicesResult<bool>> Delete(string userId, string projectId);
+        Task<ServicesResult<bool>> UpdateIsDelete(string userId, string projectId);
+        Task<ServicesResult<bool>> UpdateIsAccessed(string userId, string projectId);        
+        Task<ServicesResult<bool>> UpdateIsDone(string userId, string projectId);
+        Task<ServicesResult<bool>> UpdateStatus(string userId, string projectId);
     }
 }
