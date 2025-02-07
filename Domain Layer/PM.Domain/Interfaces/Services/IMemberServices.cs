3b221eacb6962ee4c2acfda77dfbde9c3f78112d
Akai <nguyensieu12112002@gmail.com>
2025-02-07 15:30:13 +0700

    Create IMemberServices.cs

---
+++ Domain Layer/PM.Domain/Interfaces/Services/IMemberServices.cs
@@ -0,0 +1,15 @@
+﻿using PM.Domain.Models.members;
+using Shared.member;
+
+namespace PM.Domain.Interfaces.Services
+{
+    internal interface IMemberServices
+    {
+        public Task<ServicesResult<IEnumerable<IndexMember>>> GetMembers();
+        public Task<ServicesResult<IEnumerable<IndexMember>>> GetMemberInProject(string projectId);
+        public Task<ServicesResult<DetailMember>> GetDetailMember(string memberId);
+        public Task<ServicesResult<DetailMember>> AddMember(string userId, string projectId, AddMember addMember);
+        public Task<ServicesResult<DetailMember>> UpdateMember(string userId, string memberId, UpdateMember updateMember);
+        public Task<ServicesResult<IEnumerable<IndexMember>>> DeleteMember(string userId, string memberId);
+    }
+}
