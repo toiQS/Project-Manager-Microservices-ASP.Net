d412ad46d3e63a1dbce524ac028acc6affe5676a
Akai <nguyensieu12112002@gmail.com>
2025-02-08 14:48:41 +0700

    Update AddMember.cs

--- Domain Layer/PM.Domain/Models/members/AddMember.cs
+++ Domain Layer/PM.Domain/Models/members/AddMember.cs
@@ -2,10 +2,8 @@
 {
     public class AddMember
     {
-        public string ValueUser { get; set; } = string.Empty;
-        public string PositionWorkName { get; set; } = string.Empty;
-        public string ProjectId { get; set; } = string.Empty;
-        public string RoleUserNameInProject { get; set; } = string.Empty;
-
+        public string PositionWork { get; set; }
+        public string RoleId { get; set; }
+        public string UserId { get; set; }
     }
 }
