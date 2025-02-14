b7e87c56c5603936dffe04c071cd9880f7c48b93
Akai <nguyensieu12112002@gmail.com>
2025-02-14 16:34:43 +0700

    Create IUserServices.cs

---
+++ Domain Layer/PM.Domain/Interfaces/Services/IUserServices.cs
@@ -0,0 +1,12 @@
+﻿using PM.Domain.Models.users;
+
+namespace PM.Domain.Interfaces.Services
+{
+    interface IUserServices
+    {
+        public Task<ServicesResult<DetailAppUser>> GetUserById(string userId);
+        public Task<ServicesResult<DetailAppUser>> UpdateUser(DetailAppUser user);
+        public Task<ServicesResult<bool>> DeleteUser(string userId);
+        public Task<ServicesResult<bool>> ChangePassword(ChangePasswordUser changePassword);    
+    }
+}
