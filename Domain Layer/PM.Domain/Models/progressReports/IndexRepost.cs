be696943182b9afa9ea6e1cebe45c755ade8d3a0
Akai <nguyensieu12112002@gmail.com>
2025-02-14 14:53:51 +0700

    Create IndexRepost.cs

---
+++ Domain Layer/PM.Domain/Models/progressReports/IndexRepost.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Domain.Models.progressReports
+{
+    public class IndexRepost
+    {
+        public string Id { get; set; } = string.Empty;
+        public string RepostDetail { get; set; } = string.Empty;
+        public DateOnly RepostDate { get; set; }
+    }
+}
