f8a6bc3faace068d1be2641ea31de681344c2993
Akai <nguyensieu12112002@gmail.com>
2025-02-14 16:34:35 +0700

    Create IAuthServices.cs

---
+++ Domain Layer/PM.Domain/Interfaces/Services/IAuthServices.cs
@@ -0,0 +1,13 @@
+﻿using PM.Domain.Models.auths;
+using PM.Domain.Models.users;
+
+namespace PM.Domain.Interfaces.Services
+{
+    public interface IAuthServices : IDisposable
+    {
+        public Task<ServicesResult<DetailAppUser>> LoginAsync(LoginModel model);
+        public Task<ServicesResult<DetailAppUser>> RegisterAsync(RegisterModel model);
+        public Task<ServicesResult<bool>> LogOutAsync();
+        public Task<ServicesResult<DetailAppUser>> ForgotPassword(ForgotPasswordModel model);
+    }
+}
