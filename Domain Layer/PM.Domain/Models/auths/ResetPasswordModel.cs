3a33e484ea9ef1a79c2282f32f134742fea3596b
Akai <nguyensieu12112002@gmail.com>
2025-02-14 16:34:53 +0700

    Update ResetPasswordModel.cs

--- Domain Layer/PM.Domain/Models/auths/ResetPasswordModel.cs
+++ Domain Layer/PM.Domain/Models/auths/ResetPasswordModel.cs
@@ -1,10 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.missions;
-
-namespace PM.Domain.Models.auths
+﻿namespace PM.Domain.Models.auths
 {
     public class ResetPasswordModel
     {
