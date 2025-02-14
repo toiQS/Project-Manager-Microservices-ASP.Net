7a1c121a30ba549f83b9f925edaaf4fa6ee0941a
Akai <nguyensieu12112002@gmail.com>
2025-02-14 21:52:03 +0700

    add seeding

---
+++ APIs Layer/RootAPIs/SeedData/RoleSeeding.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Identity;
+
+namespace RootAPIs.SeedData
+{
+    public static class RoleSeeding
+    {
+        public static async Task Initialize(this IServiceProvider serviceProvider)
+        {
+            try
+            {
+                var context = serviceProvider.GetRequiredService<RoleManager<IdentityRole>>();
+                if (context.Roles.Any()) return;
+                var roles = new[] { "Admin", "Customer" };
+                foreach (var role in roles)
+                {
+                    if (await context.RoleExistsAsync(role)) continue;
+                    await context.CreateAsync(new IdentityRole(role));
+                }
+
+            }
+            catch
+            {
+                throw;
+            }
+        }
+    }
+}
---
+++ APIs Layer/RootAPIs/SeedData/StatusSeeding.cs
@@ -0,0 +1,23 @@
+﻿using PM.Domain.Entities;
+using PM.Persistence;
+
+namespace RootAPIs.SeedData
+{
+    public static class StatusSeeding
+    {
+        public static async Task Initialize(this IServiceProvider serviceProvider)
+        {
+            var context = serviceProvider.GetRequiredService<ApplicationDbContext>();
+            if (context.Status.Any()) return;
+            var arr = new[] { "not selected", "waiting", "in progress", "completed early", "finished on time", "behind schedule", "finished late" };
+            foreach (var item in arr)
+            {
+                context.AddRange(new Status
+                {
+                    Name = item,
+                });
+            }
+            await context.SaveChangesAsync();
+        }
+    }
+}
