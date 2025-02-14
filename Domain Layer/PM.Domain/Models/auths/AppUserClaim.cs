759f1f400f5697712dcdb82ea2f1de5215b6312e
Akai <nguyensieu12112002@gmail.com>
2025-02-14 16:34:48 +0700

    Delete AppUserClaim.cs

--- Domain Layer/PM.Domain/Models/auths/AppUserClaim.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PM.Domain.Models.auths
-{
-    public class AppUserClaim
-    {
-        public string Email { get; set; }  = string.Empty;
-        public string RoleUser { get; set; } = string.Empty;
-    }
-}
