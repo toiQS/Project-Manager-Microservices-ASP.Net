f4530c39fb2e420c27d43b359da22d585b2fca87
Akai <nguyensieu12112002@gmail.com>
2025-02-14 14:53:49 +0700

    Update UpdatePlan.cs

--- Domain Layer/PM.Domain/Models/plans/UpdatePlan.cs
+++ Domain Layer/PM.Domain/Models/plans/UpdatePlan.cs
@@ -3,6 +3,9 @@
     public class UpdatePlan
     {
         public string PlanName { get; set; } = string.Empty;
-        public string PlanDescription { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+
+        public DateOnly StartAt { get; set; }
+        public DateOnly EndAt { get; set; }
     }
 }
