9198911d457f59c722afcda48c9fc7a6a6894c2d
Akai <nguyensieu12112002@gmail.com>
2025-02-14 21:52:28 +0700

    Create Configurations.cs

---
+++ Application Layer/PM.Application/Configurations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PM.Application.Implements;
+using PM.Application.Interfaces;
+using PM.Persistence;
+
+namespace PM.Application
+{
+    public static class Configurations
+    {
+        public static void IntializeApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            RegisterServices(services, configuration);
+            services.InitializeInfrastructer(configuration);
+        }
+        private static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddScoped<IAuthLogic, AuthLogic>();
+        }
+    }
+}
