69bd33fa820772d9a89a1b041e8fe8acace0d8ba
Akai <nguyensieu12112002@gmail.com>
2025-02-14 10:25:11 +0700

    Create IPlanServices.cs

---
+++ Domain Layer/PM.Domain/Interfaces/Services/IPlanServices.cs
@@ -0,0 +1,14 @@
+﻿using PM.Domain.Models.plans;
+
+namespace PM.Domain.Interfaces.Services
+{
+    public interface IPlanServices
+    {
+        public Task<ServicesResult<IEnumerable<IndexPlan>>> GetPlans();
+        public Task<ServicesResult<IEnumerable<IndexPlan>>> GetPlansInProject(string projectId);
+        public Task<ServicesResult<DetailPlan>> GetDetailPlan(string planId);
+        public Task<ServicesResult<DetailPlan>> AddAsync(string memberId, string projectId, AddPlan addPlan);
+        public Task<ServicesResult<DetailPlan>> UpdateAsync(string memberId, UpdatePlan updatePlan);
+        public Task<ServicesResult<IEnumerable<IndexPlan>>> DeleteAsync(string memberId, string planId);
+    }
+}
