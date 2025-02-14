042a256509ff483de64e7eddbc1d72afdf22ac94
Akai <nguyensieu12112002@gmail.com>
2025-02-14 21:52:38 +0700

    Update Program.cs

--- APIs Layer/RootAPIs/Program.cs
+++ APIs Layer/RootAPIs/Program.cs
@@ -1,9 +1,10 @@
-using PM.Persistence;
+using PM.Application;
+using RootAPIs.SeedData;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.InitializeInfrastructer(builder.Configuration);
+builder.Services.IntializeApplication(builder.Configuration);
 
 builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
@@ -19,6 +20,17 @@ if (app.Environment.IsDevelopment())
     app.UseSwaggerUI();
 }
 
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await RoleSeeding.Initialize(services);
+}
+
+using (var scope = app.Services.CreateScope())
+{
+    var services = scope.ServiceProvider;
+    await StatusSeeding.Initialize(services);
+}
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
