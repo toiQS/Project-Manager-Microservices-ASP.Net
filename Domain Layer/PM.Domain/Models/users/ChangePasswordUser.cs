d507f9b04c3676625e8f0beaadc69384b9632f57
Akai <nguyensieu12112002@gmail.com>
2025-02-14 16:34:55 +0700

    Create ChangePasswordUser.cs

---
+++ Domain Layer/PM.Domain/Models/users/ChangePasswordUser.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Domain.Models.users
+{
+    class ChangePasswordUser
+    {
+        public string Email { get; set; } = string.Empty;
+        public string OldPassword { get; set; } = string.Empty; 
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
