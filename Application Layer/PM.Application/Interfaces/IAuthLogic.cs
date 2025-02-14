9a13651018f4d58acecb84241bd6541893f07d8c
Akai <nguyensieu12112002@gmail.com>
2025-02-14 21:52:25 +0700

    Create IAuthLogic.cs

---
+++ Application Layer/PM.Application/Interfaces/IAuthLogic.cs
@@ -0,0 +1,13 @@
+﻿using PM.Domain;
+using PM.Domain.Models.auths;
+
+namespace PM.Application.Interfaces
+{
+    public interface IAuthLogic
+    {
+        public Task<string> Login(LoginModel loginModel);
+        public Task<string> Register(RegisterModel registerModel);
+        public Task<string> LogOut();
+        public Task<string> ForgotPassword(ForgotPasswordModel model);
+    }
+}
