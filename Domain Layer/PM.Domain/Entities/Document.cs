be90f075e99186b9e32f319461028dcea938d0cd
Akai <nguyensieu12112002@gmail.com>
2025-02-14 14:53:15 +0700

    Update Document.cs

--- Domain Layer/PM.Domain/Entities/Document.cs
+++ Domain Layer/PM.Domain/Entities/Document.cs
@@ -9,6 +9,7 @@ namespace PM.Domain.Entities
         public string Id { get; set; } // Mã tài liệu
         public string Name { get; set; } // Tên tài liệu
         public string Path { get; set; } // Đường dẫn tài liệu
+        public string Descriotion { get; set; } // Miêu tả tài liệu 
         [ForeignKey(nameof(Project))]
         public string ProjectId { get; set; } // Liên kết đến dự án (nếu có)
         [ForeignKey(nameof(Mission))]
