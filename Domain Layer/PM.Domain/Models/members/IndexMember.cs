3f2377f864386d8991e9d10387e4e0e79f8ac836
Akai <nguyensieu12112002@gmail.com>
2025-02-18 18:00:15 +0700

    Update IndexMember.cs

--- Domain Layer/PM.Domain/Models/members/IndexMember.cs
+++ Domain Layer/PM.Domain/Models/members/IndexMember.cs
@@ -2,7 +2,7 @@
 {
     public class IndexMember
     {
-        public string RoleUserInProjectId { get; set; } = string.Empty;
+        public string MemberId { get; set; } = string.Empty;
         public string UserName { get; set; } = string.Empty;
         public string PositionWorkName { get; set; } = string.Empty;
         public string UserAvata { get; set; } = string.Empty;
