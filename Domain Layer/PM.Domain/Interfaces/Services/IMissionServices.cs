bdeb5ca13c1b61270859daf86604f287a0086027
Akai <nguyensieu12112002@gmail.com>
2025-02-18 18:00:13 +0700

    Create IMissionServices.cs

---
+++ Domain Layer/PM.Domain/Interfaces/Services/IMissionServices.cs
@@ -0,0 +1,17 @@
+﻿using PM.Domain.Models.members;
+using PM.Domain.Models.missions;
+using PM.Domain.Models.projects;
+
+namespace PM.Domain.Interfaces.Services
+{
+    public interface IMissionServices
+    {
+        public Task<ServicesResult<IEnumerable<IndexMission>>> GetIndexMissions();
+        public Task<ServicesResult<IEnumerable<IndexMission>>> GetIndexMissionsInPlan(string planId);
+        public Task<ServicesResult<DetailMission>> GetDetailMission(string missionId);
+        public Task<ServicesResult<DetailMission>> CreateMission(string memberId, string planId ,AddMission mission);
+        public Task<ServicesResult<DetailMission>> UpdateMission(string memberId, string missionId, UpdateMission mission);
+        public Task<ServicesResult<DetailMission>> DeleteMission(string memberId, string missionId);
+
+    }
+}
