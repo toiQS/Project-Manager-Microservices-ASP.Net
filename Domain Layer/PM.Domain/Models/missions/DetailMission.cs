b3ac3d50cf3fb74633b64a12908129ab9dda2d91
Akai <nguyensieu12112002@gmail.com>
2025-02-18 18:00:23 +0700

    Create DetailMission.cs

---
+++ Domain Layer/PM.Domain/Models/missions/DetailMission.cs
@@ -0,0 +1,17 @@
+﻿using PM.Domain.Models.members;
+
+namespace PM.Domain.Models.missions
+{
+    public class DetailMission
+    {
+        public string MissionId { get; set; } = string.Empty;
+        public string MissionName { get; set; } = string.Empty;
+        public DateTime StartAt { get; set; }
+        public DateTime EndAt { get; set; }
+        public DateTime CreateAt { get; set; }
+        public string Status { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public bool IsDone { get; set; }
+        public List<IndexMember> IndexMembers { get; set; } = new List<IndexMember>();
+    }
+}
