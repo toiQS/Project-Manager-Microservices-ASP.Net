fc7cc86cc93a4a2ac92773e9efe19d55adeaaabc
Akai <nguyensieu12112002@gmail.com>
2025-02-18 18:00:28 +0700

    Create UpdateMission.cs

---
+++ Domain Layer/PM.Domain/Models/missions/UpdateMission.cs
@@ -0,0 +1,14 @@
+﻿using PM.Domain.Models.members;
+
+namespace PM.Domain.Models.missions
+{
+    public class UpdateMission
+    {
+        public string TaskName { get; set; } = string.Empty;
+        public string TaskDescription { get; set; } = string.Empty ;
+        public DateOnly StartAt { get; set; }
+        public DateOnly EndAt { get; set; }
+        //public bool IsDone { get; set; }    
+        public List<IndexMember> Members { get; set; }  = new List<IndexMember>();
+    }
+}
