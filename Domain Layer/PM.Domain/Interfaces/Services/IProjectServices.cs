8d275f8ee9599ee0b6eecc9a94d167423937157f
Akai <nguyensieu12112002@gmail.com>
2025-02-19 22:49:36 +0700

    Update IProjectServices.cs

--- Domain Layer/PM.Domain/Interfaces/Services/IProjectServices.cs
+++ Domain Layer/PM.Domain/Interfaces/Services/IProjectServices.cs
@@ -5,12 +5,12 @@ namespace PM.Domain.Interfaces.Services
     public interface IProjectServices
     {
         Task<ServicesResult<IEnumerable<IndexProject>>> GetProductListUserHasJoined(string userId);
-        //Task<ServicesResult<IEnumerable<IndexProject>>> GetProjects();
+        Task<ServicesResult<IEnumerable<IndexProject>>> GetProjects();
         Task<ServicesResult<IEnumerable<IndexProject>>> GetProjectListUserHasOwner(string userId);
         public Task<ServicesResult<DetailProject>> GetDetailProject(string projectId);
         Task<ServicesResult<DetailProject>> Add(string userId, AddProject addProject);
         Task<ServicesResult<DetailProject>> UpdateInfo(string memberId, string projectId, UpdateProject updateProject);
-        //Task<ServicesResult<IEnumerable<IndexProject>>> Delete(string memberId, string projectId);
+        Task<ServicesResult<IEnumerable<IndexProject>>> Delete(string memberId, string projectId);
         Task<ServicesResult<DetailProject>> UpdateIsDelete(string memberId, string projectId);
         Task<ServicesResult<DetailProject>> UpdateIsCompleted(string memberId, string projectId);
     }
