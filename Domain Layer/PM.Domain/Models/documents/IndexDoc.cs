0f347d87b0096d2e5a91b692227be30f1066bde4
Akai <nguyensieu12112002@gmail.com>
2025-02-20 17:17:36 +0700

    Create IndexDoc.cs

---
+++ Domain Layer/PM.Domain/Models/documents/IndexDoc.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Domain.Models.documents
+{
+    public class IndexDoc
+    {
+        public string Id { get; set; }
+        public string Name { get; set; }
+        public string Path { get; set; }
+    }
+}
