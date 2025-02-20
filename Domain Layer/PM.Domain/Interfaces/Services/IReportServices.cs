2a4775d5ec96bb5bbee6a2f4c44af2f67060814f
Akai <nguyensieu12112002@gmail.com>
2025-02-20 17:17:26 +0700

    Create IReportServices.cs

---
+++ Domain Layer/PM.Domain/Interfaces/Services/IReportServices.cs
@@ -0,0 +1,12 @@
+﻿using PM.Domain.Models.reports;
+
+namespace PM.Domain.Interfaces.Services
+{
+    public interface IReportServices
+    {
+        public Task<ServicesResult<IndexReport>> GetReportInPlan(string planId);
+        public Task<ServicesResult<string>> AddReport(string memberId, string planId, string reportDetail);
+        public Task<ServicesResult<string>> UpdateReport(string memberId, string reportId, string reportDetail);
+        public Task<ServicesResult<string>> DeleteReport(string memberId, string reportId);
+    }
+}
