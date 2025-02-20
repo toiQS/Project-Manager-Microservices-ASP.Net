4a58c56131248821f18a72785a49893639712d05
Akai <nguyensieu12112002@gmail.com>
2025-02-20 17:17:38 +0700

    Create UpdateDoc.cs

---
+++ Domain Layer/PM.Domain/Models/documents/UpdateDoc.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Domain.Models.documents
+{
+    public class UpdateDoc
+    {
+        public string Name { get; set; }
+        public string Path { get; set; }
+        public string Description { get; set; }
+    }
+}
