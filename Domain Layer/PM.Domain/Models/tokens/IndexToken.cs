5846ea493abfdc389b2227990aa5889caa76544f
Akai <nguyensieu12112002@gmail.com>
2025-02-20 17:17:47 +0700

    Create IndexToken.cs

---
+++ Domain Layer/PM.Domain/Models/tokens/IndexToken.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Domain.Models.tokens
+{
+    public class IndexToken
+    {
+        public string Id { get; set; }
+        public string UserId { get; set; }
+        public string Status { get; set; }
+    }
+}
