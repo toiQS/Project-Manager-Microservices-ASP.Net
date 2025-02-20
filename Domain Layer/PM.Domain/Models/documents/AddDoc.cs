09636152282235d678a4151d4bfb7954ea688b91
Akai <nguyensieu12112002@gmail.com>
2025-02-20 17:17:29 +0700

    Create AddDoc.cs

---
+++ Domain Layer/PM.Domain/Models/documents/AddDoc.cs
@@ -0,0 +1,12 @@
+﻿namespace PM.Domain.Models.documents
+{
+    public class AddDoc
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Path { get; set; }
+        public string ProjectId { get; set; }
+        public string PlanId { get; set; }
+        public string MissionId { get; set; }
+    }
+}
