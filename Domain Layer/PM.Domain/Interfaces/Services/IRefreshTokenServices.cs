c9d64b15c2d38d57aa8e5deefadc550c2a012515
Akai <nguyensieu12112002@gmail.com>
2025-02-20 17:17:24 +0700

    Create IRefreshTokenServices.cs

---
+++ Domain Layer/PM.Domain/Interfaces/Services/IRefreshTokenServices.cs
@@ -0,0 +1,17 @@
+﻿using PM.Domain.Entities;
+
+namespace PM.Domain.Interfaces.Services
+{
+    public interface IRefreshTokenServices
+    {
+        // thêm token vào data
+        // kiểm tra token
+        // làm mới token
+        // hủy token
+        // thông tin token
+        public Task<ServicesResult<string>> SaveToken(string userId, string token);
+        public Task<ServicesResult<string>> CheckStatusToken(string userId);
+        public Task<ServicesResult<string>> RefreshToken(string userId);
+        public Task<ServicesResult<string>> CancelToken(string userId);
+    }
+}
