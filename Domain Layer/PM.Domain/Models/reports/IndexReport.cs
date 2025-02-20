1f810d00a824fdc81cf88212b2225bd72689279b
Akai <nguyensieu12112002@gmail.com>
2025-02-20 17:17:41 +0700

    Create IndexReport.cs

---
+++ Domain Layer/PM.Domain/Models/reports/IndexReport.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Domain.Models.reports
+{
+    public class IndexReport
+    {
+        public string Id { get; set; }
+        public string ReportDetail { get; set; }
+        public DateTime DateTime { get; set; }
+    }
+}
