79401ceac91a1c637f005f635c7433da85389bf8
Akai <nguyensieu12112002@gmail.com>
2025-02-21 10:50:15 +0700

    Update IDocumentServices.cs

--- Domain Layer/PM.Domain/Interfaces/Services/IDocumentServices.cs
+++ Domain Layer/PM.Domain/Interfaces/Services/IDocumentServices.cs
@@ -8,9 +8,8 @@ namespace PM.Domain.Interfaces.Services
         public Task<ServicesResult<IEnumerable<IndexDoc>>> GetDocsInProject(string projectId);
         public Task<ServicesResult<IEnumerable<IndexDoc>>> GetDocsInPlan(string planId);
         public Task<ServicesResult<IEnumerable<IndexDoc>>> GetDocsInMission(string missionId);
-        public Task<ServicesResult<DetailDoc>> GetDocs(string docId);
+        public Task<ServicesResult<DetailDoc>> GetDoc(string docId);
         public Task<ServicesResult<DetailDoc>> AddDocToProject(string memberId, string projectId, AddDoc addDoc);
-
         public Task<ServicesResult<DetailDoc>> AddDocToMission(string memberId, string missionId, AddDoc addDoc);
         public Task<ServicesResult<DetailDoc>> UpdateDoc(string memberId, string docId, UpdateDoc updateDoc);
         public Task<ServicesResult<IEnumerable<IndexDoc>>> DeleteDoc(string memberId, string docId);
