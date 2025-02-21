8a24d7f16f423ea87bc27f8cb53e618b74b50ba0
Akai <nguyensieu12112002@gmail.com>
2025-02-21 18:07:01 +0700

    Update IReportServices.cs

--- Domain Layer/PM.Domain/Interfaces/Services/IReportServices.cs
+++ Domain Layer/PM.Domain/Interfaces/Services/IReportServices.cs
@@ -4,7 +4,7 @@ namespace PM.Domain.Interfaces.Services
 {
     public interface IReportServices
     {
-        public Task<ServicesResult<IndexReport>> GetReportInPlan(string planId);
+        public Task<ServicesResult<IEnumerable<IndexReport>>> GetReportsInPlan(string planId);
         public Task<ServicesResult<string>> AddReport(string memberId, string planId, string reportDetail);
         public Task<ServicesResult<string>> UpdateReport(string memberId, string reportId, string reportDetail);
         public Task<ServicesResult<string>> DeleteReport(string memberId, string reportId);
