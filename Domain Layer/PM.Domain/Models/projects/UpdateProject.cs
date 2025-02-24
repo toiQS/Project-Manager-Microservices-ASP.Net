62ec5b96e06a0d99d9f5780d390e3b71ec0be817
Akai <nguyensieu12112002@gmail.com>
2025-02-24 20:02:27 +0700

    Update UpdateProject.cs

--- Domain Layer/PM.Domain/Models/projects/UpdateProject.cs
+++ Domain Layer/PM.Domain/Models/projects/UpdateProject.cs
@@ -4,8 +4,8 @@
     {
         public string ProjectName { get; set; } = string.Empty;
         public string ProjectDescription { get; set; } = string.Empty;
-        public DateTime? StartDate { get; set; } // Ngày bắt đầu
-        public DateTime? EndDate { get; set; } // Ngày kết thúc
+        public DateOnly StartDate { get; set; } // Ngày bắt đầu
+        public DateOnly EndDate { get; set; } // Ngày kết thúc
 
     }
 }
