0e97b63b49f845485ab0b3ccdf1fbd0d58bd166b
Akai <nguyensieu12112002@gmail.com>
2025-02-24 20:55:11 +0700

    Create IMemberLogic.cs

---
+++ Application Layer/PM.Application/Interfaces/IMemberLogic.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PM.Application.Models.membes;
+
+namespace PM.Application.Interfaces
+{
+    public interface IMemberLogic
+    {
+        public Task<IActionResult> GetMembers();
+        public Task<IActionResult> GetMembers(string projectId);
+        public Task<IActionResult> GetMember(string memberId);
+        public Task<IActionResult> AddMember(AddMemberModel model);
+        public Task<IActionResult> DeleteMember(DeleteMemberModel model);
+        public Task<IActionResult> UpdateMember(UpdateMemberModel model);
+    }
+}
