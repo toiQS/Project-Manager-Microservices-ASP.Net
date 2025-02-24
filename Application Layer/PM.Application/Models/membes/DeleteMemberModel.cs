b1f256384d2552f717cc366f89abbc9141407112
Akai <nguyensieu12112002@gmail.com>
2025-02-24 20:55:15 +0700

    Create DeleteMemberModel.cs

---
+++ Application Layer/PM.Application/Models/membes/DeleteMemberModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PM.Application.Models.membes
+{
+public    class DeleteMemberModel
+    {
+        public string MemberCurrentId { get; set; } = string.Empty;
+        public string MemberId { get; set; } = string.Empty;
+    }
+}
