76ac324708ddf92e0ca026008057150d6ecbc744
Akai <nguyensieu12112002@gmail.com>
2025-02-24 20:55:12 +0700

    Create AddMemberModel.cs

---
+++ Application Layer/PM.Application/Models/membes/AddMemberModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PM.Application.Models.membes
+{
+   public class AddMemberModel
+    {
+        public string MemberCurrentId { get; set; } = string.Empty;
+        public string ProjectId {  get; set; } = string.Empty;
+        public string PositionWork { get; set; } = string.Empty;
+        public string RoleId { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+    }
+}
