b52b532e5d90f4c31644ea3df6024083e55311a6
Akai <nguyensieu12112002@gmail.com>
2025-02-24 20:02:19 +0700

    Create UpdateProjectModel.cs

---
+++ Application Layer/PM.Application/Models/projects/UpdateProjectModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PM.Application.Models.projects
+{
+    public class UpdateProjectModel
+    {
+        public string Token { get; set; } = string.Empty;
+        public string ProjectId { get; set; }
+        public string ProjectName { get; set; } = string.Empty;
+        public string ProjectDescription { get; set; } = string.Empty;
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+    }
+}
