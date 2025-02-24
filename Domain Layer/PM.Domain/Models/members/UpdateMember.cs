17a90b34442b82f51136a760b321c3e035c1416d
Akai <nguyensieu12112002@gmail.com>
2025-02-24 20:56:11 +0700

    Update UpdateMember.cs

--- Domain Layer/PM.Domain/Models/members/UpdateMember.cs
+++ Domain Layer/PM.Domain/Models/members/UpdateMember.cs
@@ -2,8 +2,8 @@
 {
     public class UpdateMember
     {
-        public string PositionWork {  get; set; }
-        public string ProjectId { get; set; }
-        public string UserId {  get; set; }
+        public string PositionWork { get; set; } = string.Empty;
+        public string ProjectId { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty; 
     }
 }
