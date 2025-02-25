b40984612d1aadb6aad9a3a89c9779c52174d5d2
Akai <nguyensieu12112002@gmail.com>
2025-02-25 11:15:08 +0700

    Create AddMembertMissionModel.cs

---
+++ Application Layer/PM.Application/Models/missions/AddMembertMissionModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PM.Application.Models.missions
+{
+    public class AddMembertMissionModel
+    {
+        public string MemeberId { get; set; } = string.Empty;
+        public string MissisonId { get; set; } = string.Empty;
+        public List<string> MemberIds { get; set; } = new List<string>();
+
+    }
+}
