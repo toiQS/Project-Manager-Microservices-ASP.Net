53f6bb6b255836b4238112cbdab4d59302d5d4ec
Akai <nguyensieu12112002@gmail.com>
2025-02-25 17:17:31 +0700

    Create UpdateReportModel.cs

---
+++ Application Layer/PM.Application/Models/reports/UpdateReportModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Application.Models.reports
+{
+    public class UpdateReportModel
+    {
+        public string MemberId { get; set; } = string.Empty;
+        public string ReportId { get; set; } = string.Empty;
+        public string ReportDetail { get; set; } = string.Empty;
+    }
+}
