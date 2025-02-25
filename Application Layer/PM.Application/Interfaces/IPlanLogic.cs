899d67ba48add2be962bfeea4fd1727cbb30f6c1
Akai <nguyensieu12112002@gmail.com>
2025-02-25 11:14:56 +0700

    Create IPlanLogic.cs

---
+++ Application Layer/PM.Application/Interfaces/IPlanLogic.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PM.Application.Models.plans;
+
+namespace PM.Application.Interfaces
+{
+    public interface IPlanLogic
+    {
+        public Task<IActionResult> GetPlans();
+        public Task<IActionResult> GetPlansInProject(string projectId);
+        public Task<IActionResult> GetDetailPlan(string planId);
+        public Task<IActionResult> AddPlan(AddPlanModel model);
+        public Task<IActionResult> UpdateAsync(UpdatePlanModel model);
+        public Task<IActionResult> DeleteAsync(DeletePlanModel model);
+    }
+}
