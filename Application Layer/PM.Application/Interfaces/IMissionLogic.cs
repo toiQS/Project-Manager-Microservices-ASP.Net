cc1efaa482e3d43145aa878316082944e3f5aac0
Akai <nguyensieu12112002@gmail.com>
2025-02-25 11:14:54 +0700

    Create IMissionLogic.cs

---
+++ Application Layer/PM.Application/Interfaces/IMissionLogic.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PM.Application.Models.missions;
+
+namespace PM.Application.Interfaces
+{
+    public interface IMissionLogic
+    {
+        public Task<IActionResult> GetIndexMissions();
+        public Task<IActionResult> GetIndexMissionsInPlan(string planId);
+        public Task<IActionResult> GetDetailMission(string missionId);
+        public Task<IActionResult> CreateMission(AddMissonModel model);
+        public Task<IActionResult> UpdateMission(UpdateMissionModel model);
+        public Task<IActionResult> DeleteMission(DeleteMissionModel model);
+        public Task<IActionResult> AddMembers(AddMembertMissionModel model);
+        public Task<IActionResult> DeleteMember(DeleteMemberMissionModel model);
+    }
+}
