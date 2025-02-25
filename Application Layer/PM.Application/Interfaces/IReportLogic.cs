1bb1bcdf13d449eae45a8d14b989238aaef66c5c
Akai <nguyensieu12112002@gmail.com>
2025-02-25 17:17:23 +0700

    Create IReportLogic.cs

---
+++ Application Layer/PM.Application/Interfaces/IReportLogic.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PM.Application.Models.reports;
+
+namespace PM.Application.Interfaces
+{
+    public interface IReportLogic
+    {
+        public Task<IActionResult> GetReportsInPlan(string planId);
+        public Task<IActionResult> AddReport(AddReportModel model);
+        public Task<IActionResult> UpdateReport(UpdateReportModel model);
+        public Task<IActionResult> DeleteReport(DeleteReportModel model);
+    }
+}
