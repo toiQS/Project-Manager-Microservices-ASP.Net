f7dca4b6e436b953297dd186f36da490bca114a6
Akai <nguyensieu12112002@gmail.com>
2025-02-25 11:15:23 +0700

    Create UpdatePlanModel.cs

---
+++ Application Layer/PM.Application/Models/plans/UpdatePlanModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PM.Application.Models.plans
+{
+    public class UpdatePlanModel
+    {
+        public string MemberId { get; set; } = string.Empty;
+        public string PlanId { get; set; } = string.Empty;
+        public string PlanName { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateOnly StartAt { get; set; }
+        public DateOnly EndAt { get; set; }
+    }
+}
