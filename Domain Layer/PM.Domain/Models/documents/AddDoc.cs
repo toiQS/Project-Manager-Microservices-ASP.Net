1c0802d74f92693adff0fdfcf163511ba62a930f
Akai <nguyensieu12112002@gmail.com>
2025-02-25 11:15:27 +0700

    Update AddDoc.cs

--- Domain Layer/PM.Domain/Models/documents/AddDoc.cs
+++ Domain Layer/PM.Domain/Models/documents/AddDoc.cs
@@ -5,8 +5,6 @@
         public string Name { get; set; }
         public string Description { get; set; }
         public string Path { get; set; }
-        public string ProjectId { get; set; }
-        public string PlanId { get; set; }
-        public string MissionId { get; set; }
+        
     }
 }
