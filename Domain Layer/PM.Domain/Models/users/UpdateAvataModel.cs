ac7811169a6bda9f07cac390fe702cab2b3aa263
Akai <nguyensieu12112002@gmail.com>
2025-02-25 17:17:40 +0700

    Create UpdateAvataModel.cs

---
+++ Domain Layer/PM.Domain/Models/users/UpdateAvataModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PM.Domain.Models.users
+{
+    public class UpdateAvataModel
+    {
+        public string Token { get; set; } = string.Empty;
+        public string Avata { get; set; } = string.Empty;
+    }
+}
