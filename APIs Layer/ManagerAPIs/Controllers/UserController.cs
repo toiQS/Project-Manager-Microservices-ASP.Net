4bbde1e9995767ddc4e23d2500777ff141169ce5
Akai <nguyensieu12112002@gmail.com>
2025-02-25 17:17:14 +0700

    Update UserController.cs

--- APIs Layer/ManagerAPIs/Controllers/UserController.cs
+++ APIs Layer/ManagerAPIs/Controllers/UserController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PM.Application.Interfaces;
+using PM.Application.Models.users;
 using PM.Domain.Models.users;
 
 namespace ManagerAPIs.Controllers
@@ -9,108 +10,66 @@ namespace ManagerAPIs.Controllers
     public class UserController : ControllerBase
     {
         private readonly IUserLogic _userLogic;
-        private readonly IHttpContextAccessor _httpContextAccessor;
-        private readonly ILogger<UserController> _logger;
-        public UserController(IUserLogic userLogic, IHttpContextAccessor httpContextAccessor, ILogger<UserController> logger)
+        
+        public UserController(IUserLogic userLogic)
         {
             _userLogic = userLogic;
-            _httpContextAccessor = httpContextAccessor;
-            _logger = logger;
+           
         }
+        #region GetUserDetail
+        /// <summary>
+        /// Retrieves user details using a token.
+        /// </summary>
         [HttpGet("user-detail-token")]
-        public async Task<IActionResult> GetUserDetail(string token)
+        public IActionResult GetUserDetail(string token)
         {
-            try
-            {
-                //var token = _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
-                var response = _userLogic.GetDetailUserToken(token);
-                if(response.Status == false)
-                {
-                    return BadRequest(response.Message);
-                }
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message);
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
+            return _userLogic.GetDetailUserToken(token);
         }
+        #endregion
+
+        #region GetOtherUserDetail
+        /// <summary>
+        /// Retrieves details of another user using userId.
+        /// </summary>
         [HttpGet("another-user-detail-identity")]
         public async Task<IActionResult> GetOtherUserDetail(string userId)
         {
-            try
-            {
-                //var token = _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
-                var response = await _userLogic.GetDetailUserIdentty(userId);
-                if (response.Status == false)
-                {
-                    return BadRequest(response.Message);
-                }
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message);
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
+            return await _userLogic.GetDetailUserIdentity(userId);
         }
+        #endregion
+
+        #region UpdateUser
+        /// <summary>
+        /// Updates user details.
+        /// </summary>
         [HttpPut("update-user")]
-        public async Task<IActionResult> UpdateUser(string token,[FromBody] UpdateAppUser user)
+        public async Task<IActionResult> UpdateUser(string token, [FromBody] UpdateUserModel model)
         {
-            try
-            {
-                //var token = _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
-                var response = await _userLogic.UpdateUser(token, user);
-                if (response.Status == false)
-                {
-                    return BadRequest(response.Message);
-                }
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message);
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
+            return await _userLogic.UpdateUser(model);
         }
-        [HttpPut("update-avata")]
-        public async Task<IActionResult> UpdateAvata(string token, string avata)
+        #endregion
+
+        #region UpdateAvatar
+        /// <summary>
+        /// Updates user avatar.
+        /// </summary>
+        [HttpPut("update-avatar")]
+        public async Task<IActionResult> UpdateAvatar([FromBody] UpdateAvataModel model)
         {
-            try
-            {
-                //var token = _httpContextAccessor.HttpContext.Request.Headers["Authorization"];
-                var response = await _userLogic.UpdateAvata(token, avata);
-                if (response.Status == false)
-                {
-                    return BadRequest(response.Message);
-                }
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message);
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
+            return await _userLogic.UpdateAvatar(model);
         }
+        #endregion
+
+        #region ChangePassword
+        /// <summary>
+        /// Changes user password.
+        /// </summary>
         [HttpPatch("change-password")]
-        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordUser user)
+        public async Task<IActionResult> ChangePassword([FromBody] ChangePasswordModel model)
         {
-            try
-            {
-                var response = await _userLogic.ChangePassword(user);
-                if (response.Status == false)
-                {
-                    return BadRequest(response.Message);
-                }
-                return Ok(response);
-            }
-            catch (Exception ex)
-            {
-                _logger.LogError(ex.Message);
-                return StatusCode(StatusCodes.Status500InternalServerError, ex.Message);
-            }
+            return await _userLogic.ChangePassword(model);
         }
-        
+        #endregion
+
     }
 }
