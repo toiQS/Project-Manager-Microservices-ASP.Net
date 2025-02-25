38f20875b0447f6e620d2fd6021c98a5d0e17435
Akai <nguyensieu12112002@gmail.com>
2025-02-25 11:15:09 +0700

    Create AddMissonModel.cs

---
+++ Application Layer/PM.Application/Models/missions/AddMissonModel.cs
@@ -0,0 +1,12 @@
+﻿namespace PM.Application.Models.missions
+{
+    public class AddMissonModel
+    {
+        public string MemberId { get; set; } = string.Empty;
+        public string PlanId { get; set; } = string.Empty;
+        public string TaskName { get; set; } = string.Empty;
+        public DateOnly StartAt { get; set; }
+        public DateOnly EndAt { get; set; }
+        public string Description { get; set; } = string.Empty;
+    }
+}
