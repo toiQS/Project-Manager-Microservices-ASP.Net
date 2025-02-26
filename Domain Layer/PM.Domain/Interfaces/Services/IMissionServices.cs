9475310f7e91d84638045c44c33b8f99865d4c81
Akai <nguyensieu12112002@gmail.com>
2025-02-26 20:36:07 +0700

    Update IMissionServices.cs

--- Domain Layer/PM.Domain/Interfaces/Services/IMissionServices.cs
+++ Domain Layer/PM.Domain/Interfaces/Services/IMissionServices.cs
@@ -10,6 +10,7 @@ namespace PM.Domain.Interfaces.Services
         public Task<ServicesResult<DetailMission>> CreateMission(string memberId, string planId ,AddMission mission);
         public Task<ServicesResult<DetailMission>> UpdateMission(string memberId, string missionId, UpdateMission mission);
         public Task<ServicesResult<IEnumerable<IndexMission>>> DeleteMission(string memberId, string missionId);
+        public Task<ServicesResult<bool>> DeleteMissionFunc(string memberId, string missionId);
         public Task<ServicesResult<DetailMission>> AddMembers(string memberCurrentId, string missionId, List<string> memberIds);
         public Task<ServicesResult<DetailMission>> DeleteMember(string memberCurrentId, string missionId, string memberId);
     }
