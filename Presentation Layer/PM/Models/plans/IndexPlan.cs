18c261b2a66ab015144e5ea8ff9f419493ce16bc
Akai <nguyensieu12112002@gmail.com>
2025-03-03 11:46:53 +0700

    Create IndexPlan.cs

---
+++ Presentation Layer/PM/Models/plans/IndexPlan.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Models.plans
+{
+    public class IndexPlan
+    {
+        public string PlanId { get; set; } = string.Empty;
+        public string PlanName { get; set; } = string.Empty ;
+        public string Description { get; set; } = string.Empty;
+    }
+}
