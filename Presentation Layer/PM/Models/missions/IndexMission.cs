3fbcff3ee17c3a2b558862e40a8dd19f25c399f5
Akai <nguyensieu12112002@gmail.com>
2025-03-03 11:46:49 +0700

    Create IndexMission.cs

---
+++ Presentation Layer/PM/Models/missions/IndexMission.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Models.missions
+{
+    public class IndexMission
+    {
+        public string MissionId { get; set; } = string.Empty;
+        public string MissionName { get; set; } = string.Empty ;
+        public string Status { get; set; } = string.Empty ; 
+    }
+}
