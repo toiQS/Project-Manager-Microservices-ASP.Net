1dbeacf52501490dd62ca0957be5c5042b940011
Akai <nguyensieu12112002@gmail.com>
2025-03-03 11:46:46 +0700

    Create IndexMember.cs

---
+++ Presentation Layer/PM/Models/membes/IndexMember.cs
@@ -0,0 +1,10 @@
+﻿namespace PM.Models.members
+{
+    public class IndexMember
+    {
+        public string MemberId { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string PositionWorkName { get; set; } = string.Empty;
+        public string UserAvata { get; set; } = string.Empty;
+    }
+}
