9bd5f36e42d5ff06482fd09146f364b6082680f5
Akai <nguyensieu12112002@gmail.com>
2025-03-03 10:34:03 +0700

    Create MutilProjectModel.cs

---
+++ Presentation Layer/PM/Models/projects/MutilProjectModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PM.Models.projects
+{
+public    class MutilProjectModel
+    {
+        public string Token { get; set; } =string.Empty;
+        public string ProjectId { get; set; } = string.Empty;
+    }
+}
