e80efddc9d70d0808eaf1dee9a7925fb25c6c234
Akai <nguyensieu12112002@gmail.com>
2025-03-04 17:21:46 +0700

    Create UpdateAvatarModel.cs

---
+++ Presentation Layer/PM/Models/users/UpdateAvatarModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PM.Models.users
+{
+    public class UpdateAvatarModel
+    {
+        public string Token { get; set; } = string.Empty;   
+        public string Avatar { get; set; } = string.Empty;
+    }
+}
