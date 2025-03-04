b46e93c8b307b55f95b7927715c01708d78c1d9b
Akai <nguyensieu12112002@gmail.com>
2025-03-04 17:21:44 +0700

    Create DeleteProjectModel.cs

---
+++ Presentation Layer/PM/Models/projects/DeleteProjectModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PM.Models.projects
+{
+    public class DeleteProjectModel
+    {
+        public string Token { get; set; } = string.Empty;
+        public string ProjectId {get; set; } = string.Empty;
+    }
+}
