8e9e1eb8e6bfd2eed74b45e5198a84626408ef86
Akai <nguyensieu12112002@gmail.com>
2025-03-10 12:24:02 +0700

    Update Mission.cs

--- Domain Layer/PM.Domain/Entities/Mission.cs
+++ Domain Layer/PM.Domain/Entities/Mission.cs
@@ -7,11 +7,11 @@ namespace PM.Domain.Entities
     public class Mission
     {
         [Key]
-        public string Id { get; set; } // Mã nhiệm vụ
+        public string Id { get; set; } = string.Empty;// Mã nhiệm vụ
         [ForeignKey(nameof(Plan))]
-        public string PlanId { get; set; } // Mã kế hoạch
-        public string Name { get; set; } // Tên nhiệm vụ
-        public string Description { get; set; } // Mô tả nhiệm vụ
+        public string PlanId { get; set; } = string.Empty;// Mã kế hoạch
+        public string Name { get; set; } = string.Empty;// Tên nhiệm vụ
+        public string Description { get; set; } = string.Empty;// Mô tả nhiệm vụ
         public DateTime StartDate { get; set; } // Ngày bắt đầu
         public DateTime EndDate { get; set; } // Ngày kết thúc
         public DateTime CreateDate { get; set; } // Ngày tạo
@@ -19,7 +19,7 @@ namespace PM.Domain.Entities
         public int StatusId { get; set; } // ID tình trạng
         public bool IsCompleted { get; set; } // Đã hoàn thành chưa
 
-        public Plan Plan { get; set; } // Liên kết đến kế hoạch
+        public Plan Plan { get; set; }// Liên kết đến kế hoạch
         public Status Status { get; set; } // Liên kết đến tình trạng
         public ICollection<MissionAssignment> Assignments { get; set; } // Giao nhiệm vụ
         public ICollection<Document> Documents { get; set; } // Tài liệu nhiệm vụ
