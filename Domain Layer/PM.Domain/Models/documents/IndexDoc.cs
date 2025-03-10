7316bf72dadfd25f13fdaaed698c78ec80a12555
Akai <nguyensieu12112002@gmail.com>
2025-03-10 12:24:08 +0700

    Update IndexDoc.cs

--- Domain Layer/PM.Domain/Models/documents/IndexDoc.cs
+++ Domain Layer/PM.Domain/Models/documents/IndexDoc.cs
@@ -2,8 +2,8 @@
 {
     public class IndexDoc
     {
-        public string Id { get; set; }
-        public string Name { get; set; }
-        public string Path { get; set; }
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Path { get; set; } = string.Empty;
     }
 }
