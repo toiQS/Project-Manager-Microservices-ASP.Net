fd18a4859aea36e4ae51f1c866762b48255186a7
Akai <nguyensieu12112002@gmail.com>
2025-03-12 10:56:46 +0700

    Update ProjectMember.cs

--- DomainLayers/PM.Domain/Entities/ProjectMember.cs
+++ DomainLayers/PM.Domain/Entities/ProjectMember.cs
@@ -7,16 +7,15 @@ namespace PM.Domain.Entities
     {
         [Key]
         public string Id { get; set; } // Mã thành viên dự án
-        [ForeignKey(nameof(Project))]
+        //[ForeignKey(nameof(Project))]
         public string ProjectId { get; set; } // Mã dự án
-        [ForeignKey(nameof(User))]
+
         public string UserId { get; set; } // Mã người dùng
-        [ForeignKey(nameof(RoleInProject))]
+        //[ForeignKey(nameof(RoleInProject))]
         public string RoleId { get; set; } // Vai trò trong dự án
         public string PositionWork { get; set; } = string.Empty;
 
         public Project Project { get; set; } // Liên kết đến dự án
-        public User User { get; set; } // Liên kết đến người dùng
         public RoleInProject RoleInProject { get; set; } // Liên kết đến vai trò
     }
 }
