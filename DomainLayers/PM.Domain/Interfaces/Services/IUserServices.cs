557fe4d60f69c030903e41e477e8e9d6eea4d242
Akai <nguyensieu12112002@gmail.com>
2025-03-12 10:57:19 +0700

    Update IUserServices.cs

--- DomainLayers/PM.Domain/Interfaces/Services/IUserServices.cs
+++ DomainLayers/PM.Domain/Interfaces/Services/IUserServices.cs
@@ -1,13 +1,11 @@
-﻿using PM.Domain.Models.users;
+﻿using PM.Domain.Entities;
 
 namespace PM.Domain.Interfaces.Services
 {
     public interface IUserServices
     {
-        public Task<ServicesResult<DetailAppUser>> GetDetailUser(string userId);
-        public Task<ServicesResult<DetailAppUser>> UpdateUser(string userId, UpdateAppUser user);
-        //public Task<ServicesResult<bool>> DeleteUser(string userId);
-        public Task<ServicesResult<string>> ChangePassword(ChangePasswordUser changePassword);
-        public Task<ServicesResult<DetailAppUser>> UpdateAvata(string userId, string avata);
+        public Task<ServicesResult<User>> GetDetailUser(string userId);
+        public Task<ServicesResult<bool>> UpdateUser(User user);
+        public Task<ServicesResult<bool>> PacthUser(string userId, User user);
     }
 }
