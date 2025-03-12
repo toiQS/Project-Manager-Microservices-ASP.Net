1dfa3b02b5e3e47b05ffa823774264094c8c6855
Akai <nguyensieu12112002@gmail.com>
2025-03-12 18:27:47 +0700

    Update IUnitOfWork.cs

--- DomainLayers/PM.Domain/Interfaces/IUnitOfWork.cs
+++ DomainLayers/PM.Domain/Interfaces/IUnitOfWork.cs
@@ -4,18 +4,26 @@ namespace PM.Domain.Interfaces
 {
     public interface IUnitOfWork : IDisposable
     {
-        IRepository<ActivityLog,string> ActivityLogRepository {  get; }
-        IRepository<Document,string> DocumentRepository { get; }
-        IRepository<Mission,string> MissionRepository { get; }
-        IRepository<MissionAssignment, string> MissionAssignmentRepository { get; }
-        IRepository<ProgressReport,string> ProgressReportRepository { get; }
-        IRepository<Project,string> ProjectRepository { get; }
-        IRepository<RoleInProject, string> RoleInProjectRepository { get; }
-        IRepository<ProjectMember, string> ProjectMemberRepository { get; }
-        IRepository<Status,int> StatusRepository { get; }
-        IRepository<User,string> UserRepository { get; }
-        IRepository<Plan,string> PlanRepository { get; }
-        IRepository<RefreshToken,string> RefreshTokenRepository { get; }
-        Task<int> SaveChangesAsync();
+        // Repositories
+        public IRepository<ActivityLog, string> ActivityLogRepository { get; }
+        public IRepository<Document, string> DocumentRepository { get; }
+        public IRepository<Mission, string> MissionRepository { get; }
+        public IRepository<MissionAssignment, string> MissionAssignmentRepository { get; }
+        public IRepository<ProgressReport, string> ProgressReportRepository { get; }
+        public IRepository<Project, string> ProjectRepository { get; }
+        public IRepository<RoleInProject, string> RoleInProjectRepository { get; }
+        public IRepository<ProjectMember, string> ProjectMemberRepository { get; }
+        public IRepository<Status, int> StatusRepository { get; }
+        public IRepository<User, string> UserRepository { get; }
+        public IRepository<Plan, string> PlanRepository { get; }
+        public IRepository<RefreshToken, string> RefreshTokenRepository { get; }
+
+        // Transaction & Caching
+        public Task<ServicesResult<bool>> ExecuteTransactionAsync(Func<Task> transactionOperations, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<T>> GetCachedAsync<T, TKey>(TKey id, CancellationToken cancellationToken = default) where T : class;
+
+        // SaveChanges & Event-driven
+        public Task<ServicesResult<bool>> SaveChangesAsync(CancellationToken cancellationToken = default);
+        public Task<ServicesResult<bool>> PublishEventAsync(string eventName, object eventData, CancellationToken cancellationToken = default);
     }
 }
