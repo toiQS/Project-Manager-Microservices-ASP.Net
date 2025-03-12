e1ea02c56e9e535167329bf5bde4be0c72830990
Akai <nguyensieu12112002@gmail.com>
2025-03-12 18:27:44 +0700

    Update IRepository.cs

--- DomainLayers/PM.Domain/Interfaces/IRepository.cs
+++ DomainLayers/PM.Domain/Interfaces/IRepository.cs
@@ -8,9 +8,7 @@ namespace PM.Domain.Interfaces
         // Truy vấn dữ liệu (Query Methods)
         public Task<ServicesResult<IEnumerable<T>>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken = default);
         public Task<ServicesResult<T>> GetOneByKeyAndValue(Dictionary<string, TKey> value, bool useAndOperator, CancellationToken cancellationToken = default);
-        public Task<ServicesResult<T>> GetOneByKeyAndValue(Dictionary<string, TKey> value, bool useAndOperator, CancellationToken cancellationToken = default, params Expression<Func<T, object>>[] includes);
         public Task<ServicesResult<IEnumerable<T>>> GetManyByKeyAndValue(Dictionary<string, TKey> value, bool useAndOperator, int pageNumber, int pageSize, CancellationToken cancellationToken = default);
-        public Task<ServicesResult<IEnumerable<T>>> GetManyByKeyAndValue(Dictionary<string, TKey> value, bool useAndOperator, int pageNumber, int pageSize, CancellationToken cancellationToken = default, params Expression<Func<T, object>>[] includes);
 
         // Thêm, cập nhật, xóa dữ liệu (Mutation Methods)
         public Task<ServicesResult<bool>> AddAsync(IEnumerable<T> arr, T entity, CancellationToken cancellationToken = default);
@@ -19,12 +17,5 @@ namespace PM.Domain.Interfaces
         public Task<ServicesResult<bool>> DeleteAsync(TKey primaryKey, CancellationToken cancellationToken = default);
         public Task<ServicesResult<bool>> DeleteManyAsync(Dictionary<string, TKey> value, bool useAndOperator, CancellationToken cancellationToken = default);
 
-        // Transaction & Caching
-        public Task<ServicesResult<bool>> ExecuteTransactionAsync(Func<Task> transactionOperations, CancellationToken cancellationToken = default);
-        public Task<ServicesResult<T>> GetCachedAsync(TKey id, CancellationToken cancellationToken = default);
-
-        // Event-driven & SaveChanges
-        public Task<ServicesResult<bool>> PublishEventAsync(string eventName, object eventData, CancellationToken cancellationToken = default);
-        public Task<ServicesResult<bool>> SaveChangesAsync(CancellationToken cancellationToken = default);
     }
 }
