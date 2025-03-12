16958c33eedd4f53d41a4a489a7c49b748ce05fc
Akai <nguyensieu12112002@gmail.com>
2025-03-12 10:56:49 +0700

    Update RefreshToken.cs

--- DomainLayers/PM.Domain/Entities/RefreshToken.cs
+++ DomainLayers/PM.Domain/Entities/RefreshToken.cs
@@ -1,4 +1,5 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PM.Domain.Entities
 {
@@ -7,7 +8,9 @@ namespace PM.Domain.Entities
         [Key]
         public string Id { get; set; }
         public string Token { get; set; } = string.Empty;
+        //[ForeignKey(nameof(User))]
         public string UserId { get; set; } = string.Empty;
+        public User User { get; set; }
         public DateTime Expires { get; set; }
         public bool IsExpired { get; set; }
         public bool IsRevoke { get; set; }
