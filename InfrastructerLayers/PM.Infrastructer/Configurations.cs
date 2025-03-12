e8a91009ca28b469efa732709feff6c7f3d17fea
Akai <nguyensieu12112002@gmail.com>
2025-03-12 10:57:23 +0700

    Create Configurations.cs

---
+++ InfrastructerLayers/PM.Infrastructer/Configurations.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PM.Infrastructer
+{
+    public static class Configurations
+    {
+        public static void InitializeInfrastructer(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.RegisterDatabase(configuration);
+        }
+        private static void RegisterDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<AuthDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("AuthConnectString"));
+                options.EnableSensitiveDataLogging();
+            });
+        }
+        private static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
