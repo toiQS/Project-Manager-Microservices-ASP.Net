d6296789e9d156e6e84966d2e41cd7c07dbd5bcf
Akai <nguyensieu12112002@gmail.com>
2025-03-12 10:57:02 +0700

    Update IDocumentServices.cs

--- DomainLayers/PM.Domain/Interfaces/Services/IDocumentServices.cs
+++ DomainLayers/PM.Domain/Interfaces/Services/IDocumentServices.cs
@@ -1,18 +1,18 @@
-﻿using PM.Domain.Models.documents;
+﻿using PM.Domain.Entities;
 
 namespace PM.Domain.Interfaces.Services
 {
     public interface IDocumentServices
     {
-        public Task<ServicesResult<IEnumerable<IndexDoc>>> GetDocs();
-        public Task<ServicesResult<IEnumerable<IndexDoc>>> GetDocsInProject(string projectId);
-        public Task<ServicesResult<IEnumerable<IndexDoc>>> GetDocsInPlan(string planId);
-        public Task<ServicesResult<IEnumerable<IndexDoc>>> GetDocsInMission(string missionId);
-        public Task<ServicesResult<DetailDoc>> GetDoc(string docId);
-        public Task<ServicesResult<DetailDoc>> AddDocToProject(string memberId, string projectId, AddDoc addDoc);
-        public Task<ServicesResult<DetailDoc>> AddDocToMission(string memberId, string missionId, AddDoc addDoc);
-        public Task<ServicesResult<DetailDoc>> UpdateDoc(string memberId, string docId, UpdateDoc updateDoc);
-        public Task<ServicesResult<IEnumerable<IndexDoc>>> DeleteDoc(string memberId, string docId);
-        public Task<ServicesResult<bool>> DeleteDocFunc(string memberId, string docId);
+        public Task<ServicesResult<IEnumerable<Document>>> GetDocs();
+        public Task<ServicesResult<IEnumerable<Document>>> GetDocsInProject(string projectId);
+        public Task<ServicesResult<IEnumerable<Document>>> GetDocsInPlan(string planId);
+        public Task<ServicesResult<IEnumerable<Document>>> GetDocsInMission(string missionId);
+        public Task<ServicesResult<Document>> GetDoc(string docId);
+        public Task<ServicesResult<bool>> AddDocToProject(Document newDoc);
+        public Task<ServicesResult<bool>> AddDocToMission(Document newDoc);
+        public Task<ServicesResult<bool>> UpdateDoc(Document updateDoc);
+        public Task<ServicesResult<bool>> PatchDoc(string documentId, ProjectMember updateDocc);
+        public Task<ServicesResult<bool>> DeleteDoc(string docId);
     }
 }
