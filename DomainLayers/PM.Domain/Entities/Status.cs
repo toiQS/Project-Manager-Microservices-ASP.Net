8ab090129c7b78d0eb6cedfe37cc0f55cdc694ca
Akai <nguyensieu12112002@gmail.com>
2025-03-12 10:56:52 +0700

    Update Status.cs

--- DomainLayers/PM.Domain/Entities/Status.cs
+++ DomainLayers/PM.Domain/Entities/Status.cs
@@ -7,9 +7,5 @@ namespace PM.Domain.Entities
         [Key]
         public int Id { get; set; } // Mã tình trạng
         public string Name { get; set; } // Tên tình trạng
-
-        public ICollection<Project> Projects { get; set; } // Dự án có tình trạng này
-        public ICollection<Plan> Plans { get; set; } // Kế hoạch có tình trạng này
-        public ICollection<Mission> Missions { get; set; } // Nhiệm vụ có tình trạng này
     }
 }
