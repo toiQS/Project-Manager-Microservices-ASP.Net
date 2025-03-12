083eb519675d54677a994aaafdb0637060ba600e
Akai <nguyensieu12112002@gmail.com>
2025-03-12 15:12:43 +0700

    Update Project.cs

--- DomainLayers/PM.Domain/Entities/Project.cs
+++ DomainLayers/PM.Domain/Entities/Project.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PM.Domain.Entities
@@ -6,21 +7,27 @@ namespace PM.Domain.Entities
     public class Project
     {
         [Key]
-        public string Id { get; set; } // Mã dự án
-        public string Name { get; set; } // Tên dự án
-        public string Description { get; set; } // Miêu tả dự án
-        //[ForeignKey(nameof(Status))]
-        public int StatusId { get; set; } // ID tình trạng
-        public DateTime CreatedDate { get; set; } // Ngày tạo
-        public DateTime? StartDate { get; set; } // Ngày bắt đầu
-        public DateTime? EndDate { get; set; } // Ngày kết thúc
-        public bool IsDeleted { get; set; } // Đã xóa chưa
-        public bool IsCompleted { get; set; } // Đã hoàn thành chưa
+        public string Id { get; set; } = Guid.NewGuid().ToString();
 
-        public Status Status { get; set; } // Liên kết đến tình trạng
-        public ICollection<ProjectMember> Members { get; set; } // Thành viên dự án
-        public ICollection<Plan> Plans { get; set; } // Các kế hoạch
-        //public ICollection<Document> Documents { get; set; } // Tài liệu dự án
-        //public ICollection<ActivityLog> ActivityLogs { get; set; } // Nhật ký hoạt động
+        [Required, MaxLength(255)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(1000)]
+        public string Description { get; set; } = string.Empty;
+
+        public int StatusId { get; set; }
+
+        public DateTime CreatedDate { get; set; } = DateTime.UtcNow;
+
+        public DateTime? StartDate { get; set; }
+
+        public DateTime? EndDate { get; set; }
+
+        public bool IsDeleted { get; set; } = false;
+
+        public bool IsCompleted { get; set; } = false;
+
+        [ForeignKey(nameof(StatusId))]
+        public Status Status { get; set; } = null!;
     }
 }
