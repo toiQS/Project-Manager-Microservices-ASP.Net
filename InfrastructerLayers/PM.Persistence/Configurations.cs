a4ccd9fab69f82f34725da210f72c2fa3a5d6a81
Akai <nguyensieu12112002@gmail.com>
2025-03-12 10:57:43 +0700

    Create Configurations.cs

---
+++ InfrastructerLayers/PM.Persistence/Configurations.cs
@@ -0,0 +1,26 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PM.Persistence
+{
+    public static class Configurations
+    {
+        public static void InitializePersistence(this IServiceCollection services, IConfiguration configuration)
+        {
+            RegisterDatabase(services, configuration);
+        }
+        private static void RegisterDatabase(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddDbContext<ApplicationDbContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("ProjectManagerConnectString"));
+                options.EnableSensitiveDataLogging();
+            });
+        }
+        private static void RegisterServices(this IServiceCollection services, IConfiguration configuration)
+        {
+
+        }
+    }
+}
