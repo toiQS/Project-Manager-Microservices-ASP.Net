e48d88f73a15536b5700bae5d59537f29a47392f
Akai <nguyensieu12112002@gmail.com>
2025-03-12 15:12:45 +0700

    Update ProjectMember.cs

--- DomainLayers/PM.Domain/Entities/ProjectMember.cs
+++ DomainLayers/PM.Domain/Entities/ProjectMember.cs
@@ -1,4 +1,5 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System;
+using System.ComponentModel.DataAnnotations;
 using System.ComponentModel.DataAnnotations.Schema;
 
 namespace PM.Domain.Entities
@@ -6,16 +7,24 @@ namespace PM.Domain.Entities
     public class ProjectMember
     {
         [Key]
-        public string Id { get; set; } // Mã thành viên dự án
-        //[ForeignKey(nameof(Project))]
-        public string ProjectId { get; set; } // Mã dự án
+        public string Id { get; set; } = Guid.NewGuid().ToString();
 
-        public string UserId { get; set; } // Mã người dùng
-        //[ForeignKey(nameof(RoleInProject))]
-        public string RoleId { get; set; } // Vai trò trong dự án
+        [Required]
+        public string ProjectId { get; set; } = string.Empty;
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [Required]
+        public string RoleId { get; set; } = string.Empty;
+
+        [MaxLength(255)]
         public string PositionWork { get; set; } = string.Empty;
 
-        public Project Project { get; set; } // Liên kết đến dự án
-        public RoleInProject RoleInProject { get; set; } // Liên kết đến vai trò
+        [ForeignKey(nameof(ProjectId))]
+        public Project Project { get; set; } = null!;
+
+        [ForeignKey(nameof(RoleId))]
+        public RoleInProject RoleInProject { get; set; } = null!;
     }
 }
