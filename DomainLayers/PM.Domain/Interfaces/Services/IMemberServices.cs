0b3aac8d1264f371eac7dbe1ab83d4cdc9b313c9
Akai <nguyensieu12112002@gmail.com>
2025-03-12 10:57:05 +0700

    Update IMemberServices.cs

--- DomainLayers/PM.Domain/Interfaces/Services/IMemberServices.cs
+++ DomainLayers/PM.Domain/Interfaces/Services/IMemberServices.cs
@@ -1,16 +1,15 @@
-﻿using PM.Domain.Models.members;
-using Shared.member;
+﻿using PM.Domain.Entities;
 
 namespace PM.Domain.Interfaces.Services
 {
     public interface IMemberServices
     {
-        public Task<ServicesResult<IEnumerable<IndexMember>>> GetMembers();
-        public Task<ServicesResult<IEnumerable<IndexMember>>> GetMemberInProject(string projectId);
-        public Task<ServicesResult<DetailMember>> GetDetailMember(string memberId);
-        public Task<ServicesResult<DetailMember>> AddMember(string memberCurrentId, string projectId, AddMember addMember);
-        public Task<ServicesResult<DetailMember>> UpdateMember(string memberCurrentId, string memberId, UpdateMember updateMember);
-        public Task<ServicesResult<IEnumerable<IndexMember>>> DeleteMember(string memberCurrentId, string memberId);
-        public Task<ServicesResult<bool>> DeleteMemberFunc(string memberCurrentId, string memberId);
+        public Task<ServicesResult<IEnumerable<ProjectMember>>> GetMembers();
+        public Task<ServicesResult<IEnumerable<ProjectMember>>> GetMemberInProject(string projectId);
+        public Task<ServicesResult<bool>> GetDetailMember(string memberId);
+        public Task<ServicesResult<bool>> AddMember(ProjectMember member);
+        public Task<ServicesResult<bool>> UpdateMember(ProjectMember member);
+        public Task<ServicesResult<bool>> PatchMember(string memberId, ProjectMember member);
+        public Task<ServicesResult<bool>> DeleteMember(string memberId);
     }
 }
