7a3308df0bb03eba2702fb401312d382b556eb90
Akai <nguyensieu12112002@gmail.com>
2025-03-12 10:56:29 +0700

    Update ActivityLog.cs

--- DomainLayers/PM.Domain/Entities/ActivityLog.cs
+++ DomainLayers/PM.Domain/Entities/ActivityLog.cs
@@ -7,14 +7,12 @@ namespace PM.Domain.Entities
     {
         [Key]
         public string Id { get; set; } // Mã nhật ký
-        [ForeignKey(nameof(User))]
+        //[ForeignKey(nameof(User))]
         public string UserId { get; set; } // Mã người dùng
-        [ForeignKey(nameof(Project))]
         public string? ProjectId { get; set; } // Mã dự án
         public string Action { get; set; } // Hành động thực hiện
         public DateTime ActionDate { get; set; } // Ngày thực hiện
-
         public User User { get; set; } // Liên kết đến người dùng
-        public Project? Project { get; set; } // Liên kết đến dự án
+
     }
 }
