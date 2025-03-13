ea1d8de8888dcd896f561a6f0d318c060810d978
Akai <nguyensieu12112002@gmail.com>
2025-03-13 11:56:14 +0700

    Create IQueryRepository.cs

---
+++ DomainLayers/PM.Domain/Interfaces/IQueryRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace PM.Domain.Interfaces
+{
+    public interface IQueryRepository<T, TKey> where T : class where TKey : notnull
+    {
+        // Truy vấn dữ liệu (Query Methods)
+        public Task<ServicesResult<IEnumerable<T>>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<T>> GetOneByKeyAndValue(string key, TKey value);
+        public Task<ServicesResult<T>> GetOneByKeyAndValue(Dictionary<string, TKey> value, bool useAndOperator, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<IEnumerable<T>>> GetManyByKeyAndValue(string key, TKey value);
+        public Task<ServicesResult<IEnumerable<T>>> GetManyByKeyAndValue(Dictionary<string, TKey> value, bool useAndOperator, int pageNumber, int pageSize, CancellationToken cancellationToken = default);
+    }
+}
