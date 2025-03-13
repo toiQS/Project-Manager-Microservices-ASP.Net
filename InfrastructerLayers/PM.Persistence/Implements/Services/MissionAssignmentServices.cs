b4d1cdc0575d8aa1589a639be9001355d7cc94b1
Akai <nguyensieu12112002@gmail.com>
2025-03-13 09:59:07 +0700

    Create MissionAssignmentServices.cs

---
+++ InfrastructerLayers/PM.Persistence/Implements/Services/MissionAssignmentServices.cs
@@ -0,0 +1,40 @@
+﻿using EasyNetQ.Logging;
+using PM.Domain.Entities;
+using PM.Domain;
+using PM.Domain.Interfaces;
+using Microsoft.EntityFrameworkCore.Diagnostics;
+
+namespace PM.Persistence.Implements.Services
+{
+    public class MissionAssignmentServices
+    {
+        private readonly ILogger<MissionAssignmentServices> _logger;
+        private readonly IUnitOfWork _unitOfWork;
+        public MissionAssignmentServices(ILogger<MissionAssignmentServices> logger, IUnitOfWork unitOfWork)
+        {
+            _logger = logger;
+            _unitOfWork = unitOfWork;
+        }
+        public async Task<ServicesResult<IEnumerable<MissionAssignment>>> GetMissionAssignmentsAsync()
+        {
+            var response = await _unitOfWork.MissionAssignmentRepository.GetAllAsync(1,100);
+            if (response.Status == false)
+            {
+                _logger.Error("");
+                return ServicesResult<IEnumerable<MissionAssignment>>.Failure("");
+            }
+            return ServicesResult<IEnumerable<MissionAssignment>>.Success(response.Data!);
+        }
+        //public async Task<ServicesResult<IEnumerable<MissionAssignment>>> GetMissionAssignmentsInMissionAsync(string missionId)
+        //{
+        //    var response = await _unitOfWork.MissionAssignmentRepository.GetManyByKeyAndValue(Dictionary<string, string>{
+        //        { "MissionId", missionId}
+        //    }, true)
+        //}
+        //public Task<ServicesResult<MissionAssignment>> GetMissionAssignmentAsync(string missionAssistanceId);
+        //public Task<ServicesResult<bool>> AddAsync(MissionAssignment missionAssignment);
+        //public Task<ServicesResult<bool>> AddManyAsync(List<MissionAssignment> missionAssignment);
+        //public Task<ServicesResult<bool>> RemoveAsync(MissionAssignment missionAssignment);
+        //public Task<ServicesResult<bool>> UpdateAsync(MissionAssignment missionAssignment);
+    }
+}
