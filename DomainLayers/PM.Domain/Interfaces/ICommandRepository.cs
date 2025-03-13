c07805ae7355a3fed17d31f7358c48fba2d1bf6a
Akai <nguyensieu12112002@gmail.com>
2025-03-13 11:55:52 +0700

    Create ICommandRepository.cs

---
+++ DomainLayers/PM.Domain/Interfaces/ICommandRepository.cs
@@ -0,0 +1,12 @@
+﻿namespace PM.Domain.Interfaces
+{
+    public interface ICommandRepository<T, TKey> where T : class where TKey : notnull
+    {
+        // Thêm, cập nhật, xóa dữ liệu (Mutation Methods)
+        public Task<ServicesResult<bool>> AddAsync(List<T> arr, T entity, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<bool>> UpdateAsync(List<T> arr, T entity, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<bool>> PatchAsync(List<T> arr, TKey primaryKey, Dictionary<string, object> updateValue, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<bool>> DeleteAsync(TKey primaryKey, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<bool>> DeleteManyAsync(Dictionary<string, TKey> value, bool useAndOperator, CancellationToken cancellationToken = default);
+    }
+}
