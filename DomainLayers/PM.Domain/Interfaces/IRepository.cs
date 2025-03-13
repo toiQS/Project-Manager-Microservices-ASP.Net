421c383701f7e7d7dbfc213df23d931a148b02b3
Akai <nguyensieu12112002@gmail.com>
2025-03-13 11:05:37 +0700

    Update IRepository.cs

--- DomainLayers/PM.Domain/Interfaces/IRepository.cs
+++ DomainLayers/PM.Domain/Interfaces/IRepository.cs
@@ -4,7 +4,9 @@
     {
         // Truy vấn dữ liệu (Query Methods)
         public Task<ServicesResult<IEnumerable<T>>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<T>> GetOneByKeyAndValue(string key, TKey value);
         public Task<ServicesResult<T>> GetOneByKeyAndValue(Dictionary<string, TKey> value, bool useAndOperator, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<IEnumerable<T>>> GetManyByKeyAndValue(string key, TKey value);
         public Task<ServicesResult<IEnumerable<T>>> GetManyByKeyAndValue(Dictionary<string, TKey> value, bool useAndOperator, int pageNumber, int pageSize, CancellationToken cancellationToken = default);
 
         // Thêm, cập nhật, xóa dữ liệu (Mutation Methods)
