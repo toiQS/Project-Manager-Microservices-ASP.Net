18ab87fa269a694ed312cffe255b89bcda34eef1
Akai <nguyensieu12112002@gmail.com>
2025-03-13 09:58:53 +0700

    Update IRepository.cs

--- DomainLayers/PM.Domain/Interfaces/IRepository.cs
+++ DomainLayers/PM.Domain/Interfaces/IRepository.cs
@@ -1,7 +1,4 @@
-﻿using System.Linq.Expressions;
-using System.Threading;
-
-namespace PM.Domain.Interfaces
+﻿namespace PM.Domain.Interfaces
 {
     public interface IRepository<T, TKey> where T : class where TKey : notnull
     {
@@ -11,9 +8,9 @@ namespace PM.Domain.Interfaces
         public Task<ServicesResult<IEnumerable<T>>> GetManyByKeyAndValue(Dictionary<string, TKey> value, bool useAndOperator, int pageNumber, int pageSize, CancellationToken cancellationToken = default);
 
         // Thêm, cập nhật, xóa dữ liệu (Mutation Methods)
-        public Task<ServicesResult<bool>> AddAsync(IEnumerable<T> arr, T entity, CancellationToken cancellationToken = default);
-        public Task<ServicesResult<bool>> UpdateAsync(IEnumerable<T> arr, T entity, CancellationToken cancellationToken = default);
-        public Task<ServicesResult<bool>> PatchAsync(IEnumerable<T> arr, TKey primaryKey, Dictionary<string, object> updateValue, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<bool>> AddAsync(List<T> arr, T entity, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<bool>> UpdateAsync(List<T> arr, T entity, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<bool>> PatchAsync(List<T> arr, TKey primaryKey, Dictionary<string, object> updateValue, CancellationToken cancellationToken = default);
         public Task<ServicesResult<bool>> DeleteAsync(TKey primaryKey, CancellationToken cancellationToken = default);
         public Task<ServicesResult<bool>> DeleteManyAsync(Dictionary<string, TKey> value, bool useAndOperator, CancellationToken cancellationToken = default);
 
