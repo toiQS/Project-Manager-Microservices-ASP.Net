b334d5fb8a67ff1c6304a28907f3aa340b698038
Akai <nguyensieu12112002@gmail.com>
2025-03-13 09:58:58 +0700

    Create IMissionAssignmentServices.cs

---
+++ DomainLayers/PM.Domain/Interfaces/Services/IMissionAssignmentServices.cs
@@ -0,0 +1,15 @@
+﻿using PM.Domain.Entities;
+
+namespace PM.Domain.Interfaces.Services
+{
+    public interface IMissionAssignmentServices
+    {
+        public Task<ServicesResult<IEnumerable<MissionAssignment>>> GetMissionAssignmentsAsync();
+        public Task<ServicesResult<IEnumerable<MissionAssignment>>> GetMissionAssignmentsInMissionAsync(string missionId);
+        public Task<ServicesResult<MissionAssignment>> GetMissionAssignmentAsync(string missionAssistanceId);
+        public Task<ServicesResult<bool>> AddAsync(MissionAssignment missionAssignment);
+        public Task<ServicesResult<bool>> AddManyAsync(List<MissionAssignment> missionAssignment);
+        public Task<ServicesResult<bool>> RemoveAsync(MissionAssignment missionAssignment);
+        public Task<ServicesResult<bool>> UpdateAsync(MissionAssignment missionAssignment);
+    }
+}
