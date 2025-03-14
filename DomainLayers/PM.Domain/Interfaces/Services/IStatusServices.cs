9a50e9c81e94602d53fe9ccc3afe4aa186ddae9c
Akai <nguyensieu12112002@gmail.com>
2025-03-14 13:42:57 +0700

    Create IStatusServices.cs

---
+++ DomainLayers/PM.Domain/Interfaces/Services/IStatusServices.cs
@@ -0,0 +1,20 @@
+﻿using Microsoft.Identity.Client;
+using Microsoft.VisualBasic;
+using PM.Domain.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.Domain.Interfaces.Services
+{
+    public interface IStatusServices
+    {
+        public Task<ServicesResult<Status>> GetStatusAsync(int id);
+        // current time and start time
+        public Task<ServicesResult<string>> StatusForCreateAsync(DateTime currentDate, DateTime startDate); 
+        public Task<ServicesResult<string>> StatusForUpdateAsync(DateTime startDate, DateTime endDate);
+        public Task<ServicesResult<string>> StatusForFinallyAsync(DateTime currentTime, DateTime EndDate);
+    }
+}
