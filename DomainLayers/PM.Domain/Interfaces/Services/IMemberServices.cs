b46fd88d68ccfbdb8755aafe4a0764d71364850b
Akai <nguyensieu12112002@gmail.com>
2025-03-14 16:45:40 +0700

    Update IMemberServices.cs

--- DomainLayers/PM.Domain/Interfaces/Services/IMemberServices.cs
+++ DomainLayers/PM.Domain/Interfaces/Services/IMemberServices.cs
@@ -5,7 +5,7 @@ namespace PM.Domain.Interfaces.Services
     public interface IMemberServices
     {
         public Task<ServicesResult<IEnumerable<ProjectMember>>> GetMembers();
-        public Task<ServicesResult<IEnumerable<ProjectMember>>> GetMemberInProject(string projectId);
+        public Task<ServicesResult<IEnumerable<ProjectMember>>> GetMembersInProject(string projectId);
         public Task<ServicesResult<ProjectMember>> GetDetailMember(string memberId);
         public Task<ServicesResult<bool>> AddMember(ProjectMember member);
         public Task<ServicesResult<bool>> UpdateMember(ProjectMember member);
