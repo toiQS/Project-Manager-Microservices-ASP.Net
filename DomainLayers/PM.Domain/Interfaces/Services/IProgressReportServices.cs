8c291d8626fd632b9fef051f435f9a76e2625a14
Akai <nguyensieu12112002@gmail.com>
2025-03-14 16:45:43 +0700

    Create IProgressReportServices.cs

---
+++ DomainLayers/PM.Domain/Interfaces/Services/IProgressReportServices.cs
@@ -0,0 +1,14 @@
+﻿using PM.Domain.Entities;
+
+namespace PM.Domain.Interfaces.Services
+{
+    public interface IProgressReportServices
+    {
+        public Task<ServicesResult<IEnumerable<ProgressReport>>> GetReports();
+        public Task<ServicesResult<IEnumerable<ProgressReport>>> GetReportsInPlan(string planId);
+        public Task<ServicesResult<bool>> AddAsync(ProgressReport progressReport);
+        public Task<ServicesResult<bool>> UpdateAsync(ProgressReport progressReport);
+        public Task<ServicesResult<bool>> PatchAsync(string progressReportId, ProgressReport progressReport);
+        public Task<ServicesResult<bool>> DeleteAsync(string progressReportId);
+    }
+}
