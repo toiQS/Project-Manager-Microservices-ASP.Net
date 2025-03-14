3537b9e44fd60c91502b29e56d85b280a497670c
Akai <nguyensieu12112002@gmail.com>
2025-03-14 16:45:48 +0700

    Update IProjectServices.cs

--- DomainLayers/PM.Domain/Interfaces/Services/IProjectServices.cs
+++ DomainLayers/PM.Domain/Interfaces/Services/IProjectServices.cs
@@ -2,20 +2,54 @@
 
 namespace PM.Domain.Interfaces.Services
 {
-    public interface
-        IProjectServices
+    /// <summary>
+    /// Service interface for managing projects.
+    /// </summary>
+    public interface IProjectServices
     {
+        /// <summary>
+        /// Retrieves all projects.
+        /// </summary>
+        /// <returns>A collection of projects.</returns>
         public Task<ServicesResult<IEnumerable<Project>>> GetProjectsAsync();
-        public Task<ServicesResult<IEnumerable<Project>>> GetProjectsUserHasJoined(string userId);
-        public Task<ServicesResult<IEnumerable<Project>>> GetProjectsUserIsOwner(string userId);
-        public Task<ServicesResult<IEnumerable<Project>>> GetProjectsUserIsLeader(string userId);
-        public Task<ServicesResult<IEnumerable<Project>>> GetProjectsUserIsManager(string userId);
-        public Task<ServicesResult<IEnumerable<Project>>> GetProjectsUserIsMember(string userId);
-        public Task<ServicesResult<IEnumerable<Project>>> GetProjectsUserIsOrtherPosition(string userId);
+
+        /// <summary>
+        /// Retrieves a specific project by its unique identifier.
+        /// </summary>
+        /// <param name="projectId">The unique identifier of the project.</param>
+        /// <returns>The project associated with the provided ID.</returns>
         public Task<ServicesResult<Project>> GetProjectAsync(string projectId);
-        public Task<ServicesResult<bool>> CreateProjectAsync(Project project);
-        public Task<ServicesResult<bool>> UpdateProjectAsync(Project project);
-        public Task<ServicesResult<bool>> PatchProjectAsync(string projectId, Project project);
+
+        /// <summary>
+        /// Creates a new project.
+        /// </summary>
+        /// <param name="arr">A collection of projects to which the new project will be added.</param>
+        /// <param name="project">The project to create.</param>
+        /// <returns>True if the operation was successful; otherwise, false.</returns>
+        public Task<ServicesResult<bool>> CreateProjectAsync(IEnumerable<Project> arr, Project project);
+
+        /// <summary>
+        /// Updates an existing project.
+        /// </summary>
+        /// <param name="arr">A collection of projects for reference during the update.</param>
+        /// <param name="project">The updated project data.</param>
+        /// <returns>True if the update was successful; otherwise, false.</returns>
+        public Task<ServicesResult<bool>> UpdateProjectAsync(IEnumerable<Project> arr, Project project);
+
+        /// <summary>
+        /// Applies partial updates to a project.
+        /// </summary>
+        /// <param name="arr">A collection of projects for reference during the update.</param>
+        /// <param name="projectId">The unique identifier of the project to patch.</param>
+        /// <param name="project">The project data to patch.</param>
+        /// <returns>True if the patch was successful; otherwise, false.</returns>
+        public Task<ServicesResult<bool>> PatchProjectAsync(IEnumerable<Project> arr, string projectId, Project project);
+
+        /// <summary>
+        /// Deletes a project.
+        /// </summary>
+        /// <param name="projectId">The unique identifier of the project to delete.</param>
+        /// <returns>True if the deletion was successful; otherwise, false.</returns>
         public Task<ServicesResult<bool>> DeleteProjectAsync(string projectId);
     }
 }
