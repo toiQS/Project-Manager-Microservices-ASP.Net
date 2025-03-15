c7798dbeda3733a6536b6af361b4b9888e55d477
Akai <nguyensieu12112002@gmail.com>
2025-03-15 09:29:56 +0700

    Update IQueryRepository.cs

--- DomainLayers/PM.Domain/Interfaces/IQueryRepository.cs
+++ DomainLayers/PM.Domain/Interfaces/IQueryRepository.cs
@@ -5,8 +5,8 @@
         // Truy vấn dữ liệu (Query Methods)
         public Task<ServicesResult<IEnumerable<T>>> GetAllAsync(int pageNumber, int pageSize, CancellationToken cancellationToken = default);
         public Task<ServicesResult<T>> GetOneByKeyAndValue(string key, TKey value);
-        public Task<ServicesResult<T>> GetOneByKeyAndValue(Dictionary<string, TKey> value, bool useAndOperator, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<T>> GetOneByKeyAndValue(Dictionary<string, string> value, bool useAndOperator, CancellationToken cancellationToken = default);
         public Task<ServicesResult<IEnumerable<T>>> GetManyByKeyAndValue(string key, TKey value);
-        public Task<ServicesResult<IEnumerable<T>>> GetManyByKeyAndValue(Dictionary<string, TKey> value, bool useAndOperator, int pageNumber, int pageSize, CancellationToken cancellationToken = default);
+        public Task<ServicesResult<IEnumerable<T>>> GetManyByKeyAndValue(Dictionary<string, string> keyValuePairs, bool useAndOperator, int pageNumber, int pageSize, CancellationToken cancellationToken = default);
     }
 }
