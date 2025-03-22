f40effbb21e9fe32b9716fa6fa5947ddb63e5ac3
Akai <nguyensieu12112002@gmail.com>
2025-03-22 12:08:55 +0700

    Create IAuthUnitOfWork.cs

---
+++ DomainLayers/PM.Domain/Interfaces/IAuthUnitOfWork.cs
@@ -0,0 +1,15 @@
+﻿using PM.Domain.Entities;
+
+namespace PM.Domain.Interfaces
+{
+    public interface IAuthUnitOfWork
+    {
+        IQueryRepository<ActivityLog, string> ActivityLogQueryRepository { get; }
+        IQueryRepository<RefreshToken, string> RefreshTokenQueryRepoitory { get; } 
+        IQueryRepository<User, string> UserQueryRepository { get; }
+        ICommandRepository<ActivityLog, string> ActivityLogCommandRepository { get; }
+        ICommandRepository<RefreshToken, string> RefreshTokenCommandRepoitory { get; }
+        ICommandRepository<User, string> UserCommandRepository { get; }
+        
+    }
+}
