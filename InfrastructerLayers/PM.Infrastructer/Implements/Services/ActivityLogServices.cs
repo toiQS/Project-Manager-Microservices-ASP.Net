40013207fbfb187ff01c48c35feaa4bc880a0221
Akai <nguyensieu12112002@gmail.com>
2025-03-22 13:22:14 +0700

    Update ActivityLogServices.cs

--- InfrastructerLayers/PM.Infrastructer/Implements/Services/ActivityLogServices.cs
+++ InfrastructerLayers/PM.Infrastructer/Implements/Services/ActivityLogServices.cs
@@ -1,12 +1,49 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using Microsoft.Extensions.Logging;
+using PM.Domain.Entities;
+using PM.Domain;
+using PM.Domain.Interfaces;
+using System.Data.Entity.Core.Metadata.Edm;
 
 namespace PM.Infrastructer.Implements.Services
 {
     public class ActivityLogServices
     {
+        private readonly IAuthUnitOfWork _authUnitOfWork;
+        private readonly ILogger<ActivityLogServices> _logger;
+        public ActivityLogServices(IAuthUnitOfWork authUnitOfWork, ILogger<ActivityLogServices> logger)
+        {
+            _authUnitOfWork = authUnitOfWork;
+            _logger = logger;
+        }
+        public async Task<ServicesResult<bool>> AddAsync(ActivityLog log)
+        {
+            if (log == null)
+            {
+                _logger.LogWarning("");
+                return ServicesResult<bool>.Failure("");
+            }
+            try
+            {
+                var logs = await _authUnitOfWork.ActivityLogQueryRepository.GetManyByKeyAndValue("UserId", log.UserId);
+                if (logs.Status == false)
+                {
+                    _logger.LogError("");
+                    return ServicesResult<bool>.Failure("");
+                }
+                var addResponse = await _authUnitOfWork.ActivityLogCommandRepository.AddAsync(logs.Data!.ToList(), log);
+                if (addResponse.Status == false)
+                {
+                    _logger.LogError("");
+                    return ServicesResult<bool>.Failure("");
+                }
+                _logger.LogInformation("");
+                return ServicesResult<bool>.Success(true);
+            }
+            catch (Exception ex)
+            {
+                _logger.LogError("");
+                return ServicesResult<bool>.Failure("");
+            }
+        }
     }
 }
