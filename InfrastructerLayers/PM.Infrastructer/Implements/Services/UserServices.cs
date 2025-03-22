9c5e865924ce24569d4971230d3b31406d6fee39
Akai <nguyensieu12112002@gmail.com>
2025-03-22 13:22:21 +0700

    Create UserServices.cs

---
+++ InfrastructerLayers/PM.Infrastructer/Implements/Services/UserServices.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.Extensions.Logging;
+using PM.Domain.Entities;
+using PM.Domain;
+using PM.Domain.Interfaces;
+
+namespace PM.Infrastructer.Implements.Services
+{
+    public class UserServices
+    {
+        private readonly IAuthUnitOfWork _authUnitOfWork;
+        private readonly ILogger<UserServices> _logger;
+        public UserServices(IAuthUnitOfWork authUnitOfWork, ILogger<UserServices> logger)
+        {
+            _authUnitOfWork = authUnitOfWork;
+            _logger = logger;
+        }
+        public Task<ServicesResult<User>> GetDetailUser(string userId)
+        {
+            if (userId == null)
+            {
+                _logger.LogError("");
+            }
+        }
+        public Task<ServicesResult<bool>> UpdateUser(User user);
+        public Task<ServicesResult<bool>> PacthUser(string userId, User user);
+    }
+}
