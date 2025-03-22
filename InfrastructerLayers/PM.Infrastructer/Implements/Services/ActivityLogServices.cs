29ca983e4deb1988fcc31fd5248f46b563165109
Akai <nguyensieu12112002@gmail.com>
2025-03-22 12:09:53 +0700

    Create ActivityLogServices.cs

---
+++ InfrastructerLayers/PM.Infrastructer/Implements/Services/ActivityLogServices.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.Infrastructer.Implements.Services
+{
+    public class ActivityLogServices
+    {
+    }
+}
