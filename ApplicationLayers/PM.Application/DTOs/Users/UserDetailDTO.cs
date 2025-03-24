aba4ce9f9043abb351cc96c7f9f76fb945241710
Akai <nguyensieu12112002@gmail.com>
2025-03-24 11:25:08 +0700

    Update UserDetailDTO.cs

--- ApplicationLayers/PM.Application/DTOs/Users/UserDetailDTO.cs
+++ ApplicationLayers/PM.Application/DTOs/Users/UserDetailDTO.cs
@@ -3,7 +3,7 @@
     public class UserDetailDTO
     {
         public string UserId { get; set; } = string.Empty;
-        public string Role { get; set; } = string.Empty;
+        //public string Role { get; set; } = string.Empty;
         public string UserName { get; set; } = string.Empty;
         public string Email { get; set; } = string.Empty;
         public string FullName { get; set; } = string.Empty;
