b2b8f5271d653b63ae0775152a58115894e87a0a
Akai <nguyensieu12112002@gmail.com>
2025-03-24 11:25:26 +0700

    Create IAuthFlowLogic.cs

---
+++ ApplicationLayers/PM.Application/Interfaces/IAuthFlowLogic.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using PM.Application.Features.Auth.Commands;
+
+namespace PM.Application.Interfaces
+{
+    public interface IAuthFlowLogic
+    {
+        public Task<IActionResult> Login(LoginCommand loginCommand);
+
+        public Task<IActionResult> Register(RegisterCommand command);
+    }
+}
