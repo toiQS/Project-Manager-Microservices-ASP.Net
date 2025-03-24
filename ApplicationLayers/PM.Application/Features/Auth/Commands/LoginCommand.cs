9299b81a5b765733ef2b281f28357eecb0ca9d86
Akai <nguyensieu12112002@gmail.com>
2025-03-24 07:48:07 +0700

    Create LoginCommand.cs

---
+++ ApplicationLayers/PM.Application/Features/Auth/Commands/LoginCommand.cs
@@ -0,0 +1,8 @@
+﻿namespace PM.Application.Features.Auth.Commands
+{
+    public class LoginCommand
+    {
+        public string Email { get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
