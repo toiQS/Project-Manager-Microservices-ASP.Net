5d9621aa06e9ebdd28717736dfbcef8d44d4046e
Akai <nguyensieu12112002@gmail.com>
2025-03-24 11:25:12 +0700

    Create RegisterCommand.cs

---
+++ ApplicationLayers/PM.Application/Features/Auth/Commands/RegisterCommand.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Application.Features.Auth.Commands
+{
+    public class RegisterCommand
+    {
+        public string Username { get; set; } = string.Empty;
+        public string Email {  get; set; } = string.Empty;
+        public string Password { get; set; } = string.Empty;
+    }
+}
