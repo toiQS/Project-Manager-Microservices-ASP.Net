293efa5c6eaaf853b661b3917f4af25566c240cd
Akai <nguyensieu12112002@gmail.com>
2025-03-24 11:25:15 +0700

    Create PacthUserCommand.cs

---
+++ ApplicationLayers/PM.Application/Features/Users/Commands/PacthUserCommand.cs
@@ -0,0 +1,13 @@
+﻿namespace PM.Application.Features.Users.Commands
+{
+    public class PacthUserCommand
+    {
+        public string UserName { get; set; } = string.Empty;
+        public string Email = string.Empty;
+        public string NumberPhone {  get; set; } = string.Empty;
+        public string AvatarUrl { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+    }
+}
