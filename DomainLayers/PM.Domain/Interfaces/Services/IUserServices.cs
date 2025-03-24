a732db6965791c1e6f4a50ba6069a38b7a7cd674
Akai <nguyensieu12112002@gmail.com>
2025-03-24 07:48:20 +0700

    Update IUserServices.cs

--- DomainLayers/PM.Domain/Interfaces/Services/IUserServices.cs
+++ DomainLayers/PM.Domain/Interfaces/Services/IUserServices.cs
@@ -4,8 +4,8 @@ namespace PM.Domain.Interfaces.Services
 {
     public interface IUserServices
     {
-        public Task<ServicesResult<User>> GetDetailUser(string userId);
-        public Task<ServicesResult<bool>> UpdateUser(User user);
-        public Task<ServicesResult<bool>> PacthUser(string userId, User user);
+        public Task<ServicesResult<User>> GetDetailUserAsync(string userId);
+        public Task<ServicesResult<bool>> UpdateUserAsync(User user);
+        public Task<ServicesResult<bool>> PatchUserAsync(string userId, User user);
     }
 }
