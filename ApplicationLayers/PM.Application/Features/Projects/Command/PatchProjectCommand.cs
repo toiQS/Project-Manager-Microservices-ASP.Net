c16c20175044564abb4b2d6d3b8d6feb29f17ecd
Akai <nguyensieu12112002@gmail.com>
2025-04-01 00:44:40 +0700

    Create PatchProjectCommand.cs

---
+++ ApplicationLayers/PM.Application/Features/Projects/Command/PatchProjectCommand.cs
@@ -0,0 +1,14 @@
+﻿namespace PM.Application.Features.Projects.Command
+{
+    public class PatchProjectCommand
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string ProjectId { get; set; } = string.Empty;
+        public string ProjectName { get; set; } = string.Empty;
+        public string ProjectDescription { get; set; } = string.Empty;
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+        public bool IsDelete { get; set; }
+        public bool IsComplete { get; set; }
+    }
+}
