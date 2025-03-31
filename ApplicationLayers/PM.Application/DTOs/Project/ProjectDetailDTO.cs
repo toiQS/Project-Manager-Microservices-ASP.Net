1f1e089f99d01e11044b9bd4244e36dd05ca5b35
Akai <nguyensieu12112002@gmail.com>
2025-04-01 00:44:28 +0700

    Create ProjectDetailDTO.cs

---
+++ ApplicationLayers/PM.Application/DTOs/Project/ProjectDetailDTO.cs
@@ -0,0 +1,17 @@
+﻿namespace PM.Application.DTOs.Project
+{
+    public class ProjectDetailDTO
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;    
+        public string Description { get; set; } = string.Empty;
+        public string StatusInfo {  get; set; } = string.Empty;
+        public DateTime CreatedDate { get; set; } 
+
+        public DateTime StartDate { get; set; }
+
+        public DateTime EndDate { get; set; }
+        public string DeleteInfo { get; set; } = string.Empty;
+        public string CompleteInfo { get; set; } = string.Empty;
+    }
+}
