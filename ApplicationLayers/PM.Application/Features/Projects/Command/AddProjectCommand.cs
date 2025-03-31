dee8bce90494202b10053404a79135ca383075d3
Akai <nguyensieu12112002@gmail.com>
2025-04-01 00:44:33 +0700

    Update AddProjectCommand.cs

--- ApplicationLayers/PM.Application/Features/Projects/Command/AddProjectCommand.cs
+++ ApplicationLayers/PM.Application/Features/Projects/Command/AddProjectCommand.cs
@@ -2,7 +2,7 @@ namespace PM.Application.Features.Projects.Command
 {
     public class AddProjectCommand
     {
-        public string Token { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
         public string ProjectName { get; set; } = string.Empty;
         public string ProjectDescription { get; set; } = string.Empty;
         public DateOnly StartAt { get; set; }
