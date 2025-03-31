e82eef58544f8811ef7df02ea93cbc1dc655e26d
Akai <nguyensieu12112002@gmail.com>
2025-04-01 00:44:30 +0700

    Create ProjectIndexDTO.cs

---
+++ ApplicationLayers/PM.Application/DTOs/Project/ProjectIndexDTO.cs
@@ -0,0 +1,13 @@
+﻿namespace PM.Application.DTOs.Project
+{
+    public class ProjectIndexDTO
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        
+        public DateTime CreatedDate { get; set; }
+
+        public string OwnerName { get; set; } = string.Empty.ToString();
+
+    }
+}
