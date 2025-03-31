d9052069dcb5d74b5c7f65a3f4f695dc240933b5
Akai <nguyensieu12112002@gmail.com>
2025-03-31 09:07:12 +0700

    Create PacthProjectCommand.cs

---
+++ ApplicationLayers/PM.Application/Features/Projects/Command/PacthProjectCommand.cs
@@ -0,0 +1,12 @@
+﻿namespace PM.Application.Features.Projects.Command
+{
+    public class PacthProjectCommand
+    {
+        public string Token { get; set; } = string.Empty;
+        public string ProjectId { get; set; } = string.Empty;
+        public string ProjectName { get; set; } = string.Empty;
+        public string ProjectDescription { get; set; } = string.Empty;
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+    }
+}
