ead847d3271bd2df99ce577fe3c75eac06b68ee3
Akai <nguyensieu12112002@gmail.com>
2025-04-02 07:57:26 +0700

    Create ProjectMember.cs

---
+++ Shared/Core/Entities/Projects/ProjectMember.cs
@@ -0,0 +1,18 @@
+﻿using Shared.Core.Entities.Base;
+using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shared.Core.Entities.Projects
+{
+    public class ProjectMember : BaseEntity
+    {
+        public Guid ProjectId { get; set; }
+        public Guid UserId { get; set; }
+        public Guid RoleId { get; set; }
+
+        [MaxLength(255)]
+        public string PositionWork { get; set; } = string.Empty;
+    }
+
+}
