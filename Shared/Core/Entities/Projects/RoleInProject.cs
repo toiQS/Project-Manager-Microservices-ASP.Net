f35ed68a7ed9d31baf8a7b16be01e2757ef944c7
Akai <nguyensieu12112002@gmail.com>
2025-04-02 07:57:30 +0700

    Create RoleInProject.cs

---
+++ Shared/Core/Entities/Projects/RoleInProject.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Core.Entities.Projects
+{
+    public class RoleInProject
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required, MaxLength(255)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(1000)]
+        public string Description { get; set; } = string.Empty;
+    }
+}
