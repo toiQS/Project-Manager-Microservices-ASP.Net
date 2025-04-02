41a3cd104251be67158773bbd58d2842a5b260b8
Akai <nguyensieu12112002@gmail.com>
2025-04-02 07:57:07 +0700

    Create AuditableEntity .cs

---
+++ Shared/Core/Entities/Base/AuditableEntity .cs
@@ -0,0 +1,8 @@
+﻿namespace Shared.Core.Entities.Base
+{
+    public abstract class AuditableEntity : BaseEntity
+    {
+        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
+        public DateTime? UpdatedAt { get; set; }
+    }
+}
