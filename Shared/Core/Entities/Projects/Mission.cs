2a8b2f4fd84a403e2a845ce931083456cf77f629
Akai <nguyensieu12112002@gmail.com>
2025-04-02 07:57:14 +0700

    Create Mission.cs

---
+++ Shared/Core/Entities/Projects/Mission.cs
@@ -0,0 +1,23 @@
+﻿using Shared.Core.Entities.Base;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shared.Core.Entities.Projects
+{
+    public class Mission : AuditableEntity
+    {
+        public Guid PlanId { get; set; }
+
+        [Required, MaxLength(255)]
+        public string Name { get; set; } = string.Empty;
+
+        [MaxLength(1000)]
+        public string Description { get; set; } = string.Empty;
+
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+
+        public bool IsCompleted { get; set; } = false;
+    }
+
+}
