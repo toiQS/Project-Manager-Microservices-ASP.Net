4d48bf834075f04f9fcce3ddca3cfd958ac89570
Akai <nguyensieu12112002@gmail.com>
2025-04-02 07:57:35 +0700

    Create RefreshToken.cs

---
+++ Shared/Core/Entities/Users/RefreshToken.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shared.Core.Entities.Users
+{
+    public class RefreshToken
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string Token { get; set; } = string.Empty;
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(UserId))]
+        public User User { get; set; } = null!;
+
+        public DateTime Expires { get; set; }
+
+        public bool IsExpired => DateTime.UtcNow >= Expires;
+
+        public bool IsRevoke { get; set; } = false;
+    }
+}
