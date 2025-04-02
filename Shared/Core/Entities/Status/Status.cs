bbf63c66d2823ffe8d898389ce55b879faeca669
Akai <nguyensieu12112002@gmail.com>
2025-04-02 07:57:32 +0700

    Create Status.cs

---
+++ Shared/Core/Entities/Status/Status.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Shared.Core.Entities.Status
+{
+    public class Status
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required, MaxLength(255)]
+        public string Name { get; set; } = string.Empty;
+    }
+}
