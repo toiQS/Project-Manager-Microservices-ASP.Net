604376606bf909d2f4048a2d51e421d284a8b20b
Akai <nguyensieu12112002@gmail.com>
2025-04-02 07:57:05 +0700

    Create UrlHelper.cs

---
+++ Shared/Core/CommonUtils/UrlHelper.cs
@@ -0,0 +1,21 @@
+﻿namespace Shared.Core.CommonUtils
+{
+    using System.Web;
+
+    public static class UrlHelper
+    {
+        public static string Encode(string url) => HttpUtility.UrlEncode(url);
+        public static string Decode(string url) => HttpUtility.UrlDecode(url);
+
+        public static string AppendQueryString(string url, Dictionary<string, string> queryParams)
+        {
+            var uriBuilder = new UriBuilder(url);
+            var query = HttpUtility.ParseQueryString(uriBuilder.Query);
+            foreach (var param in queryParams)
+                query[param.Key] = param.Value;
+            uriBuilder.Query = query.ToString();
+            return uriBuilder.ToString();
+        }
+    }
+
+}
