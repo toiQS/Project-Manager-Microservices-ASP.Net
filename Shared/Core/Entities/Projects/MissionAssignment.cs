4547945a082692e5f11ba0646fdf88f3d4056d31
Akai <nguyensieu12112002@gmail.com>
2025-04-02 07:57:17 +0700

    Create MissionAssignment.cs

---
+++ Shared/Core/Entities/Projects/MissionAssignment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Shared.Core.Entities.Projects
+{
+    public class MissionAssignment
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string MissionId { get; set; } = string.Empty;
+
+        [Required]
+        public string ProjectMemberId { get; set; } = string.Empty;
+
+        [ForeignKey(nameof(MissionId))]
+        public Mission Mission { get; set; } = null!;
+
+        [ForeignKey(nameof(ProjectMemberId))]
+        public ProjectMember ProjectMember { get; set; } = null!;
+    }
+}
