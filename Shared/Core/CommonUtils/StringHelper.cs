957ac57014e67406f5b41a9609a963175b51c172
Akai <nguyensieu12112002@gmail.com>
2025-04-02 07:57:02 +0700

    Create StringHelper.cs

---
+++ Shared/Core/CommonUtils/StringHelper.cs
@@ -0,0 +1,23 @@
+﻿using System.Globalization;
+using System.Text.RegularExpressions;
+
+namespace Shared.Core.CommonUtils
+{
+    public static class StringHelper
+    {
+        public static string ToTitleCase(string input)
+        {
+            if (string.IsNullOrWhiteSpace(input)) return input;
+            return CultureInfo.CurrentCulture.TextInfo.ToTitleCase(input.ToLower());
+        }
+
+        public static bool IsNullOrWhiteSpace(string input) => string.IsNullOrWhiteSpace(input);
+
+        public static string GenerateSlug(string text)
+        {
+            text = text.ToLower().Replace(" ", "-");
+            return Regex.Replace(text, "[^a-z0-9-]", "");
+        }
+    }
+
+}
