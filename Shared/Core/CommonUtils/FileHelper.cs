acba1d5ff9dfe7ee7d5fa702747973cc15ed846f
Akai <nguyensieu12112002@gmail.com>
2025-04-02 07:56:55 +0700

    Create FileHelper.cs

---
+++ Shared/Core/CommonUtils/FileHelper.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Shared.Core.CommonUtils
+{
+    public static class FileHelper
+    {
+        public static async Task WriteToFileAsync(string filePath, string content)
+        {
+            await File.WriteAllTextAsync(filePath, content);
+        }
+
+        public static async Task<string> ReadFromFileAsync(string filePath)
+        {
+            return await File.ReadAllTextAsync(filePath);
+        }
+
+        public static bool FileExists(string filePath)
+        {
+            return File.Exists(filePath);
+        }
+    }
+
+}
