796e0e7db41d05027414ea791646585c77f07029
Akai <nguyensieu12112002@gmail.com>
2025-04-05 00:06:38 +0700

    Create ContentLog.cs

---
+++ Shared/PM.Shared.Dtos/ContentLog.cs
@@ -0,0 +1,12 @@
+﻿namespace PM.Shared.Dtos
+{
+    public class ContentLog
+    {
+        public string ActionName { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string Message { get; set; } = string.Empty;
+        public bool Status { get; set; } = true;   
+        public int StatusCode { get; set; } = 200;
+        public DateTime ActionAt { get; set; } = DateTime.UtcNow;
+    }
+}
