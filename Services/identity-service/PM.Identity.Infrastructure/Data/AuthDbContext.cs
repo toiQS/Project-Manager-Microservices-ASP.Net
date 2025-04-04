27396e3ed3f00a59f7e0f61a90be641b3dc92cd6
Akai <nguyensieu12112002@gmail.com>
2025-04-05 00:11:09 +0700

    Create AuthDbContext.cs

---
+++ Services/identity-service/PM.Identity.Infrastructure/Data/AuthDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using PM.Identity.Domain.Entities;
+
+namespace PM.Identity.Infrastructure.Data
+{
+    public class AuthDbContext : IdentityDbContext
+    {
+        public AuthDbContext(DbContextOptions<AuthDbContext> options) : base(options) { }
+        public DbSet<User> Users {  get; set; }
+        public DbSet<RefreshToken> RefreshTokens { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+            foreach (var entityType in modelBuilder.Model.GetEntityTypes())
+            {
+                var tableName = entityType.GetTableName();
+                if (tableName != null && tableName.StartsWith("AspNet"))
+                {
+                    entityType.SetTableName(tableName.Substring(6));
+                }
+            }
+        }
+    }
+}
