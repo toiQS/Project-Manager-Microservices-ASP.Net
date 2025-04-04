5cc77656c70e1b46fc079005dc30cea49d9b30d2
Akai <nguyensieu12112002@gmail.com>
2025-04-05 00:09:25 +0700

    Delete AuditableEntity .cs

--- Shared/Core/Entities/Base/AuditableEntity .cs
+++
@@ -1,8 +0,0 @@
-﻿namespace Shared.Core.Entities.Base
-{
-    public abstract class AuditableEntity : BaseEntity
-    {
-        public DateTime CreatedAt { get; set; } = DateTime.UtcNow;
-        public DateTime? UpdatedAt { get; set; }
-    }
-}
