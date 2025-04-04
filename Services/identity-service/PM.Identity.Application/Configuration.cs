9f199273502754e6c86dbf857901c1751937a832
Akai <nguyensieu12112002@gmail.com>
2025-04-05 00:07:35 +0700

    Create Configuration.cs

---
+++ Services/identity-service/PM.Identity.Application/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PM.Identity.Infrastructure.Config;
+
+namespace PM.Identity.Application
+{
+    public static class Configuration
+    {
+        public static void InitializeInfrastructureIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.InitializeSQL(configuration);
+            services.InitializeIdentity();
+            services.InitializeJwt(configuration);
+        }
+    }
+}
