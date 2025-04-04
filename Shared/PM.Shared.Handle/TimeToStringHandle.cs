fddfc47fcd3266849dc0656680a8c10ed88a3d53
Akai <nguyensieu12112002@gmail.com>
2025-04-05 00:06:27 +0700

    Create TimeToStringHandle.cs

---
+++ Shared/PM.Shared.Handle/TimeToStringHandle.cs
@@ -0,0 +1,12 @@
+﻿namespace PM.Shared.Handle
+{
+    public class TimeToStringHandle
+    {
+        private readonly DateTime now = DateTime.UtcNow;
+        public string TimeToString()
+        {
+            var time = now.ToString("dddd-dd-MM-yyyy-HH-mm-ss-ff-tt");
+            return time;
+        }
+    }
+}
