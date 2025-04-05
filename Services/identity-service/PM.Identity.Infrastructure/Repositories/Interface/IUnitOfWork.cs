bfa36cbaf08d52c296486a0be5fef0f06aa29be3
Akai <nguyensieu12112002@gmail.com>
2025-04-05 11:33:47 +0700

    Create IUnitOfWork.cs

---
+++ Services/identity-service/PM.Identity.Infrastructure/Repositories/Interface/IUnitOfWork.cs
@@ -0,0 +1,13 @@
+﻿using PM.Identity.Infrastructure.Repositories.Implement;
+using PM.Shared.Dtos;
+
+namespace PM.Identity.Infrastructure.Repositories.Interface
+{
+    public interface IUnitOfWork
+    {
+        public IAuthRepository AuthRepository { get; }
+
+
+        public Task<ServiceResult<bool>> ExecuteTransactionAsync(Func<ServiceResult<bool>> transactionOperations, CancellationToken cancellationToken = default);
+    }
+}
