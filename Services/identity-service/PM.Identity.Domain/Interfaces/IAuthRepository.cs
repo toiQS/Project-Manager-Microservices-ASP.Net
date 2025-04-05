85bff85ff08cb43c0ea464876c93891dedf36a1a
Akai <nguyensieu12112002@gmail.com>
2025-04-05 11:33:29 +0700

    Delete IAuthRepository.cs

--- Services/identity-service/PM.Identity.Domain/Interfaces/IAuthRepository.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PM.Identity.Domain.Interfaces
-{
-    public interface IAuthRepository
-    {
-        public Task<bool> Login(string email, string password);
-        public Task<bool> Logout(string email, string password);
-        public Task<bool> Register(string email, string username, string password);
-    }
-}
