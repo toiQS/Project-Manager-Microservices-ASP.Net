80dddc31268db21b1af30f7519b44bc9c8a1732b
Akai <nguyensieu12112002@gmail.com>
2025-04-05 11:55:39 +0700

    Create ActivityLog.cs

---
+++ Services/tracking-service/PM.Tracking.Domain/Entities/ActivityLog.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace PM.Tracking.Domain.Entities
+{
+    public class ActivityLog
+    {
+        [Key]
+        public string Id { get; set; } = Guid.NewGuid().ToString();
+
+        [Required]
+        public string UserId { get; set; } = string.Empty;
+
+        public string? ProjectId { get; set; }
+
+        [Required, MaxLength(500)]
+        public string Action { get; set; } = string.Empty;
+
+        public DateTime ActionDate { get; set; } = DateTime.UtcNow;
+    }
+}
