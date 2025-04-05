e167b0c1ed02f2f47568c79238b5a4b9116907ea
Akai <nguyensieu12112002@gmail.com>
2025-04-05 11:33:45 +0700

    Create IAuthRepository.cs

---
+++ Services/identity-service/PM.Identity.Infrastructure/Repositories/Interface/IAuthRepository.cs
@@ -0,0 +1,13 @@
+﻿using PM.Shared.Dtos;
+
+namespace PM.Identity.Infrastructure.Repositories.Interface
+{
+    public interface IAuthRepository
+    {
+        public Task<ServiceResult<bool>> Login(string email, string password);
+        public Task<ServiceResult<bool>> Register(string email, string username, string password);
+        public Task<ServiceResult<bool>> Logout();
+        public Task<ServiceResult<bool>> ChangePassword(string email, string oldPassword, string newPassword);
+        
+    }
+}
