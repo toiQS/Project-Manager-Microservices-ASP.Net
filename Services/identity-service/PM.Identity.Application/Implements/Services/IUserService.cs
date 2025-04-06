4f7bb18dfcc24287394c6b41fac10cc8721d63df
Akai <nguyensieu12112002@gmail.com>
2025-04-06 09:34:13 +0700

    Create IUserService.cs

---
+++ Services/identity-service/PM.Identity.Application/Implements/Services/IUserService.cs
@@ -0,0 +1,10 @@
+﻿using PM.Identity.Domain.Entities;
+using PM.Shared.Dtos;
+
+namespace PM.Identity.Application.Implements.Services
+{
+    public interface IUserService
+    {
+        //public Task<ServiceResult<User>> 
+    }
+}
