3d1c14bf357b5cebdfa40b926332033c66c5f7c2
Akai <nguyensieu12112002@gmail.com>
2025-04-06 21:55:15 +0700

    Create IAuthFlow.cs

---
+++ Services/identity-service/PM.Identity.Application/Implements/Flows/IAuthFlow.cs
@@ -0,0 +1,10 @@
+﻿using PM.Shared.Dtos.Auths;
+using PM.Shared.Dtos;
+
+namespace PM.Identity.Application.Implements.Flows
+{
+    public interface IAuthFlow
+    {
+        public Task<ServiceResult<string>> Login(LoginModel loginModel);
+    }
+}
