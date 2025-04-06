874d889ef942d5bbe8ec66b889748563cd60fb64
Akai <nguyensieu12112002@gmail.com>
2025-04-06 21:55:42 +0700

    Create ITokenService.cs

---
+++ Services/identity-service/PM.Identity.Application/Interfaces/Services/ITokenService.cs
@@ -0,0 +1,9 @@
+﻿using PM.Identity.Domain.Entities;
+
+namespace PM.Identity.Application.Interfaces.Services
+{
+    public interface ITokenService
+    {
+        public string GenerateAccessToken(User user);
+    }
+}
