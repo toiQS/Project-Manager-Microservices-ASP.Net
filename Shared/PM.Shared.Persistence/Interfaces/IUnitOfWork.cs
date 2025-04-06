e82e58324095da3e39f17a2a02ef883ef975d37e
Akai <nguyensieu12112002@gmail.com>
2025-04-06 09:34:46 +0700

    Create IUnitOfWork.cs

---
+++ Shared/PM.Shared.Persistence/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PM.Identity.Domain.Entities;
+using PM.Shared.Dtos;
+
+namespace PM.Shared.Persistence.Interfaces
+{
+    public interface IUnitOfWork<TData> where TData : DbContext
+    {
+        public IRepository<TData, RefreshToken> RefreshTokenRepository { get; }
+        public Task<ServiceResult<bool>> ExecuteTransactionAsync(Func<Task> transactionOperations, CancellationToken cancellationToken = default);
+    }
+}
