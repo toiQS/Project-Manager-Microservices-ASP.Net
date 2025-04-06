0ddd80339b2e9e13105ce9f230369466d3b66bf0
Akai <nguyensieu12112002@gmail.com>
2025-04-06 21:55:33 +0700

    Create IAuthService.cs

---
+++ Services/identity-service/PM.Identity.Application/Interfaces/Services/IAuthService.cs
@@ -0,0 +1,12 @@
+﻿using PM.Shared.Dtos;
+
+namespace PM.Identity.Application.Interfaces.Services
+{
+    public interface IAuthService
+    {
+        public Task<ServiceResult<bool>> Login(string email, string password);
+        public Task<ServiceResult<bool>> Logout();
+        public Task<ServiceResult<bool>> Register(string email, string username, string password);
+        public Task<ServiceResult<bool>> ChangePassword(string email, string oldPassword, string newPassword);
+    }
+}
