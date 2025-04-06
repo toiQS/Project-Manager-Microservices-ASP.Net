ffb78919913f0a7a6bb997a1b38cf5f6d90eab04
Akai <nguyensieu12112002@gmail.com>
2025-04-06 21:55:37 +0700

    Create IRefreshTokenService.cs

---
+++ Services/identity-service/PM.Identity.Application/Interfaces/Services/IRefreshTokenService.cs
@@ -0,0 +1,13 @@
+﻿using PM.Identity.Domain.Entities;
+using PM.Shared.Dtos;
+
+namespace PM.Identity.Application.Interfaces.Services
+{
+    public interface IRefreshTokenService
+    {
+        public Task<ServiceResult<bool>> FindTokensUserIsNotRevokeThenPactchIsRevoke(string userId);
+        public Task<ServiceResult<bool>> AddRefreshTokenAsync(RefreshToken refreshToken);
+        public Task<ServiceResult<bool>> PacthAsync(RefreshToken refreshToken, Dictionary<string, object> keyValuePairs);
+        public Task<ServiceResult<RefreshToken>> GetRefreshTokenByToken(string token);
+    }
+}
