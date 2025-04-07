55859f97cd5ada12377cddd4ead3926844d494a0
Akai <nguyensieu12112002@gmail.com>
2025-04-07 16:08:27 +0700

    Create AuthController.cs

---
+++ Services/identity-service/PM.Identity.API/Controllers/AuthController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PM.Identity.API.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class AuthController : ControllerBase
+    {
+        [HttpGet("demo")]
+        public IActionResult Get()
+        {
+            return Ok("AuthController is working");
+        }
+    }
+}
