8801054c2170b795408a6ae9715992406448e2f0
Akai <nguyensieu12112002@gmail.com>
2025-04-07 22:17:12 +0700

    Update IAuthFlow.cs

--- Services/identity-service/PM.Identity.Application/Implements/Flows/IAuthFlow.cs
+++ Services/identity-service/PM.Identity.Application/Implements/Flows/IAuthFlow.cs
@@ -1,10 +1,14 @@
 ﻿using PM.Shared.Dtos.Auths;
 using PM.Shared.Dtos;
+using System.Threading.Tasks;
 
 namespace PM.Identity.Application.Implements.Flows
 {
     public interface IAuthFlow
     {
         public Task<ServiceResult<string>> Login(LoginModel loginModel);
+        public Task<ServiceResult<string>> Register(RegisterModel model);
+        public Task<ServiceResult<string>> LogOut(string token);
+        public Task<ServiceResult<string>> ChangePassword(string email, string oldPassword, string newPassword);
     }
 }
