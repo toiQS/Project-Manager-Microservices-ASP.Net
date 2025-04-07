a3a11e2355b6f2ab6a790d8033481ccdc131205f
Akai <nguyensieu12112002@gmail.com>
2025-04-07 22:17:07 +0700

    Update AuthController.cs

--- Services/identity-service/PM.Identity.API/Controllers/AuthController.cs
+++ Services/identity-service/PM.Identity.API/Controllers/AuthController.cs
@@ -1,5 +1,7 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PM.Identity.Application.Implements.Flows;
+using PM.Shared.Dtos.Auths;
 
 namespace PM.Identity.API.Controllers
 {
@@ -7,10 +9,32 @@ namespace PM.Identity.API.Controllers
     [ApiController]
     public class AuthController : ControllerBase
     {
-        [HttpGet("demo")]
-        public IActionResult Get()
+        private readonly IAuthFlow _authFlow;
+        private readonly ILogger<AuthController> _logger;
+        public AuthController(IAuthFlow authFlow, ILogger<AuthController> logger)
         {
-            return Ok("AuthController is working");
+            _authFlow = authFlow;
+            _logger = logger;
+        }
+        [HttpPost("login")]
+        public async Task<IActionResult> Login([FromBody] LoginModel loginModel)
+        {
+            var result = await _authFlow.Login(loginModel);
+            if (result.Status)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
+        }
+        [HttpPost("register")]
+        public async Task<IActionResult> Register([FromBody] RegisterModel registerModel)
+        {
+            var result = await _authFlow.Register(registerModel);
+            if (result.Status)
+            {
+                return Ok(result.Data);
+            }
+            return BadRequest(result.Message);
         }
     }
 }
