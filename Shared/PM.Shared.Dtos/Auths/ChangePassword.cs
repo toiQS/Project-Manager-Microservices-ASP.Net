b53bc41254bc333afbd3ef86b3a0f81b37c11a7c
Akai <nguyensieu12112002@gmail.com>
2025-04-07 22:48:06 +0700

    Create ChangePassword.cs

---
+++ Shared/PM.Shared.Dtos/Auths/ChangePassword.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Shared.Dtos.Auths
+{
+    public class ChangePassword
+    {
+        public string Email { get; set; } = string.Empty;
+        public string OldPassword { get; set; } = string.Empty;
+        public string NewPassword { get; set; } = string.Empty;
+    }
+}
