32cd88ebc81f9d36022b05e6a659fb629dcf30bb
Akai <nguyensieu12112002@gmail.com>
2025-04-08 08:58:58 +0700

    Create UserFlow.cs

---
+++ Services/identity-service/PM.Identity.Application/Implements/Flows/UserFlow.cs
@@ -0,0 +1,14 @@
+﻿using PM.Identity.Application.Interfaces.Services;
+
+namespace PM.Identity.Application.Implements.Flows
+{
+    public class UserFlow
+    {
+        private readonly IUserService _userService;
+        public UserFlow(IUserService userService)
+        {
+            _userService = userService;
+        }
+        //public string 
+    }
+}
