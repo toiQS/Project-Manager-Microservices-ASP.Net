69b063a31e26f0e6a4e7cb6954b728975485729c
Akai <nguyensieu12112002@gmail.com>
2025-04-08 08:59:11 +0700

    Update Program.cs

--- gateway/PM.Hosting/Program.cs
+++ gateway/PM.Hosting/Program.cs
@@ -2,4 +2,5 @@ var builder = DistributedApplication.CreateBuilder(args);
 var identityService = builder.AddProject<Projects.PM_Identity_API>("identity-service");
 var projectService = builder.AddProject<Projects.PM_Project_API>("project-service");
 builder.AddProject<Projects.PM_EndPoint_API>("pm-endpoint-api");
+builder.AddProject<Projects.PM_Gateway_API>("pm-gateway-api");
 builder.Build().Run();
