9ebe7bc0ef7c280c8efc37ec81c8a466bc169844
Akai <nguyensieu12112002@gmail.com>
2025-04-10 18:03:08 +0700

    Create UpdateUserModel.cs

---
+++ Shared/PM.Shared.Dtos/Users/UpdateUserModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PM.Shared.Dtos.Users
+{
+    public class UpdateUserModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string AvatarPath { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+    }
+}
