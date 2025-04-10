82193dceb72c23cd707aa213458428aa7aa54945
Akai <nguyensieu12112002@gmail.com>
2025-04-10 18:03:05 +0700

    Create TokenHandle.cs

---
+++ Shared/PM.Shared.Handle/TokenHandle.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using PM.Identity.Domain.Entities;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
+
+namespace PM.Shared.Handle
+{
+    public class TokenHandle 
+    {
+  
+        private readonly IConfiguration _configuration;
+        public TokenHandle(IConfiguration configuration)
+        {
+            _configuration = configuration;
+            
+        }
+        public string GenerateAccessToken(User user)
+        {
+            var claims = new[]
+            {
+                new Claim(ClaimTypes.NameIdentifier, user.Id),
+                new Claim(ClaimTypes.Email, user.Email!),
+                new Claim(ClaimTypes.Name, user.UserName!),
+            };
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]!));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256Signature);
+            var tokenOptions = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+                signingCredentials: signinCredentials
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            return tokenString;
+        }
+    }
+}
