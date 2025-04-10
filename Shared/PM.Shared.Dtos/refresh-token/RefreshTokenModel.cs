ca055e5fbd321b53285e208884041c6721a72147
Akai <nguyensieu12112002@gmail.com>
2025-04-10 18:03:17 +0700

    Create RefreshTokenModel.cs

---
+++ Shared/PM.Shared.Dtos/refresh-token/RefreshTokenModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PM.Shared.Dtos.refresh_token
+{
+    public class RefreshTokenModel
+    {
+        public string Token { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+    }
+}
