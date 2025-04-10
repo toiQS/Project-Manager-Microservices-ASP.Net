1292287036ebceef880c2e5582d210bc64690f44
Akai <nguyensieu12112002@gmail.com>
2025-04-10 18:04:17 +0700

    Update Program.cs

--- gateway/PM.Gateway.API/Program.cs
+++ gateway/PM.Gateway.API/Program.cs
@@ -2,7 +2,10 @@ using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
-builder.Configuration.AddJsonFile("Ocelot/ocelot.json", optional: false, reloadOnChange: true);
+
+builder.Configuration.AddJsonFile("Ocelot/IdentityOcelot.json", optional: false, reloadOnChange: true);
+//builder.Configuration.AddJsonFile("Ocelot/UserOcelot.json", optional: false, reloadOnChange: true);
+
 builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddControllers();
 var app = builder.Build();
