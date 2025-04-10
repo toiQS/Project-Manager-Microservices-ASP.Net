1969cb7b3c8ad1043bf1d02ebcc8393dd8e3ae97
Akai <nguyensieu12112002@gmail.com>
2025-04-10 18:04:36 +0700

    Create Configuration.cs

---
+++ EndPoints/PM.EndPoint.API/Configuration.cs
@@ -0,0 +1,16 @@
+﻿using PM.EndPoint.API.Flows.Implements;
+using PM.EndPoint.API.Flows.Interfaces;
+
+namespace PM.EndPoint.API
+{
+    public static class Configuration
+    {
+        public static void InitializeFlow(this IServiceCollection serviceDescriptors, IConfiguration configuration)
+        {
+            serviceDescriptors.AddHttpClient();
+            serviceDescriptors.AddHttpContextAccessor();
+            serviceDescriptors.AddScoped<IAuthFlow, AuthFlow>();
+
+        }
+    }
+}
