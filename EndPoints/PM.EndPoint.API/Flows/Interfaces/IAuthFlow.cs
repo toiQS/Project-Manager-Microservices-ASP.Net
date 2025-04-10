1df7859bb74d314523ad92e469c02dcbe782d324
Akai <nguyensieu12112002@gmail.com>
2025-04-10 18:04:20 +0700

    Create IAuthFlow.cs

---
+++ EndPoints/PM.EndPoint.API/Flows/Interfaces/IAuthFlow.cs
@@ -0,0 +1,11 @@
+﻿using PM.Shared.Dtos.Auths;
+
+namespace PM.EndPoint.API.Flows.Interfaces
+{
+    public interface IAuthFlow
+    {
+        public Task<string> HandleLogin(LoginModel loginModel);
+        //public Task<string> HandleRegister(RegisterModel registerModel);
+        public Task<string> HandleDemo();
+    }
+}
