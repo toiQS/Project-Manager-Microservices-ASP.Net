dc2247eed69f37b3f61e4f13921a9fd890e1544c
Akai <nguyensieu12112002@gmail.com>
2025-04-14 14:54:18 +0700

    Update Program.cs

--- EndPoints/PM.EndPoint.API/Program.cs
+++ EndPoints/PM.EndPoint.API/Program.cs
@@ -1,3 +1,4 @@
+using Microsoft.OpenApi.Models;
 using PM.EndPoint.API;
 
 var builder = WebApplication.CreateBuilder(args);
@@ -10,7 +11,35 @@ builder.Services.AddOpenApi();
 
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddOpenApiDocument();
-builder.Services.AddSwaggerGen();
+builder.Services.AddHttpContextAccessor();
+//builder.Services.AddSwaggerGen();
+
+builder.Services.AddSwaggerGen(c =>
+{
+    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PM.Identity API", Version = "v1" });
+
+    var securityScheme = new OpenApiSecurityScheme
+    {
+        Name = "Authorization",
+        Type = SecuritySchemeType.Http,
+        Scheme = "bearer",
+        BearerFormat = "JWT",
+        In = ParameterLocation.Header,
+        Description = "JWT Authorization header using the Bearer scheme.",
+        Reference = new OpenApiReference
+        {
+            Type = ReferenceType.SecurityScheme,
+            Id = "Bearer"
+        }
+    };
+
+    c.AddSecurityDefinition("Bearer", securityScheme);
+    c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { securityScheme, Array.Empty<string>() }
+    });
+});
+
 
 var app = builder.Build();
 
