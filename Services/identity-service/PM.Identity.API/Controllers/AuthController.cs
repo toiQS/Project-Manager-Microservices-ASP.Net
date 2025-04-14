f056cae3abd11b3cf78e5c579639fb8d639a02ba
Akai <nguyensieu12112002@gmail.com>
2025-04-14 14:54:29 +0700

    Update AuthController.cs

--- Services/identity-service/PM.Identity.API/Controllers/AuthController.cs
+++ Services/identity-service/PM.Identity.API/Controllers/AuthController.cs
@@ -1,6 +1,12 @@
 ﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 using PM.Identity.Application.Interfaces;
+using PM.Identity.Domain.Entities;
+using PM.Shared.Dtos;
 using PM.Shared.Dtos.Auths;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 [Route("api/[controller]")]
 [ApiController]
@@ -8,19 +14,30 @@ public class AuthController : ControllerBase
 {
     private readonly IAuthService _authServices;
     private readonly ILogger<AuthController> _logger;
-    public AuthController(IAuthService authServices, ILogger<AuthController> logger)
+    private readonly IConfiguration _configuration;
+    private readonly IHttpContextAccessor _httpContextAccessor;
+    public AuthController(IAuthService authServices, ILogger<AuthController> logger, IConfiguration configuration, IHttpContextAccessor httpContextAccessor)
     {
+        _configuration = configuration;
         _authServices = authServices;
         _logger = logger;
+        _httpContextAccessor = httpContextAccessor;
     }
     [HttpPost("login")]
     public async Task<IActionResult> HandleLogin([FromBody] LoginModel request)
     {
         _logger.LogInformation("Login request received for email: {Email}", request.Email);
         var result = await _authServices.SignInAsync(request.Email, request.Password);
-       if(result.Status)
-        {
-            return Ok(result);
+        if (result.Status)
+        { 
+            var user = new User()
+            {
+                UserName = "akai",
+                Email = request.Email,
+                Id = Guid.NewGuid().ToString()
+            };
+            var token = GenerateToken(user);
+            return Ok(token);
         }
         else
         {
@@ -67,6 +84,56 @@ public class AuthController : ControllerBase
         else
         {
             return BadRequest(result);
+        }
+    }
+    [HttpGet("get-user")]
+    public IActionResult GetUser()
+    {
+        var user = _httpContextAccessor.HttpContext?.User;
+        if (user.Identity.IsAuthenticated)
+        {
+            return Ok(new
+            {
+                Id = user.FindFirst(ClaimTypes.NameIdentifier)?.Value,
+                Email = user.FindFirst(ClaimTypes.Email)?.Value,
+                UserName = user.FindFirst(ClaimTypes.Name)?.Value
+            });
+        }
+        return Unauthorized("User not authenticated");
+    }
+
+    private string GenerateToken(User user)
+    {
+        try
+        {
+            if (user is null)
+            {
+                return "User not found";
+            }
+
+            var claims = new[]
+            {
+                    new Claim(ClaimTypes.NameIdentifier, user.Id.ToString()),
+                    new Claim(ClaimTypes.Email, user.Email!),
+                    new Claim(ClaimTypes.Name, user.UserName!),
+                };
+
+            var secretKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(_configuration["Jwt:SecretKey"]!));
+            var signinCredentials = new SigningCredentials(secretKey, SecurityAlgorithms.HmacSha256Signature);
+            var tokenOptions = new JwtSecurityToken(
+                issuer: _configuration["Jwt:Issuer"],
+                audience: _configuration["Jwt:Audience"],
+                claims: claims,
+                expires: DateTime.Now.AddMinutes(30),
+            signingCredentials: signinCredentials
+            );
+
+            var tokenString = new JwtSecurityTokenHandler().WriteToken(tokenOptions);
+            return tokenString;
+        }
+        catch (Exception ex)
+        {
+            return "Token generation failed";
         }
     }
 }
