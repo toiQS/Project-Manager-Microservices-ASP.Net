00f9b11124c62ce3dff847313cbae458a81d6509
Akai <nguyensieu12112002@gmail.com>
2025-04-14 14:54:34 +0700

    Update Configuration.cs

--- Services/identity-service/PM.Identity.Application/Configuration.cs
+++ Services/identity-service/PM.Identity.Application/Configuration.cs
@@ -1,4 +1,6 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using PM.Identity.Application.Implements;
 using PM.Identity.Application.Interfaces;
@@ -15,34 +17,82 @@ namespace PM.Identity.Application
         public static void InitializeInfrastructureIdentity(this IServiceCollection services, IConfiguration configuration)
         {
             services.InitializeSQL(configuration);
-            services.InitializeIdentity();
+            services.InitializeIdentity(configuration);
             services.InitializeRepository(configuration);
             services.InitializeServices(configuration);
             services.SeedingData(configuration).GetAwaiter().GetResult();
         }
-        private static void InitializeRepository(this IServiceCollection serviceDescriptors, IConfiguration configuration)
+        private static void InitializeRepository(this IServiceCollection services, IConfiguration configuration)
         {
-            serviceDescriptors.AddScoped<IRepository<AuthDbContext, User>, Repository<AuthDbContext, User>>();
-            serviceDescriptors.AddScoped<IRepository<AuthDbContext, RefreshToken>, Repository<AuthDbContext, RefreshToken>>();
+            services.AddScoped<IRepository<AuthDbContext, User>, Repository<AuthDbContext, User>>();
+            services.AddScoped<IRepository<AuthDbContext, RefreshToken>, Repository<AuthDbContext, RefreshToken>>();
 
-            serviceDescriptors.AddScoped<IUnitOfWork<AuthDbContext>, UnitOfWork<AuthDbContext>>();
+            services.AddScoped<IUnitOfWork<AuthDbContext>, UnitOfWork<AuthDbContext>>();
         }
-        private static void InitializeServices(this IServiceCollection serviceDescriptors, IConfiguration configuration)
+        private static void InitializeServices(this IServiceCollection services, IConfiguration configuration)
         {
-            serviceDescriptors.AddScoped<IAuthService, AuthService>();
-            serviceDescriptors.AddScoped<IUserService, UserService>();
+            services.AddScoped<IAuthService, AuthService>();
+            services.AddScoped<IUserService, UserService>();
             
-            serviceDescriptors.AddScoped<IRefreshTokenService, RefreshTokenService>();
+            services.AddScoped<IRefreshTokenService, RefreshTokenService>();
         }
-        private static async Task SeedingData(this IServiceCollection serviceDescriptors, IConfiguration configuration)
+        private static async Task SeedingData(this IServiceCollection services, IConfiguration configuration)
         {
-            var app = serviceDescriptors.BuildServiceProvider();
+            var app = services.BuildServiceProvider();
             using (var scope = app.CreateScope())
             {
-               var services = scope.ServiceProvider;
-                await RoleSeeding.Initialize(services);
+               var seedingResult = scope.ServiceProvider;
+                await RoleSeeding.Initialize(seedingResult);
             }
         }
-        
+        private static void InitializeIdentity(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.Configure<IdentityOptions>(options =>
+            {
+                // Password settings
+                options.Password.RequireDigit = false;
+                options.Password.RequiredLength = 1;
+                options.Password.RequireNonAlphanumeric = false;
+                options.Password.RequireUppercase = false;
+                options.Password.RequireLowercase = false;
+                // Lockout settings
+                options.Lockout.AllowedForNewUsers = true;
+                options.Lockout.DefaultLockoutTimeSpan = TimeSpan.FromMinutes(5);
+                options.Lockout.MaxFailedAccessAttempts = 5;
+                // User settings
+                options.User.RequireUniqueEmail = true;
+            });
+
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+                
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration.GetSection("Jwt:Issuer").Value,
+                    ValidAudiences = configuration.GetSection("Jwt:Audiences").Get<string[]>(),
+                    IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration.GetSection("Jwt:SecretKey").Value!))
+                };
+                
+            });
+
+            services.AddIdentity<User, IdentityRole>(options =>
+            {
+                options.User.RequireUniqueEmail = true;
+                options.Password.RequiredLength = 1;
+                options.Password.RequireDigit = false;
+                options.Password.RequireLowercase = false;
+                options.Password.RequireUppercase = false;
+            })
+                .AddEntityFrameworkStores<AuthDbContext>()
+                .AddDefaultTokenProviders();
+        }
     }
 }
