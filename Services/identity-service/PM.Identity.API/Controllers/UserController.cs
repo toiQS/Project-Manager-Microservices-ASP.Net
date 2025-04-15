1c98c6b7af0633d6c9a3a199dd95459f1fc2c6a8
Akai <nguyensieu12112002@gmail.com>
2025-04-15 18:07:51 +0700

    Update UserController.cs

--- Services/identity-service/PM.Identity.API/Controllers/UserController.cs
+++ Services/identity-service/PM.Identity.API/Controllers/UserController.cs
@@ -1,7 +1,5 @@
-﻿using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Mvc;
+﻿using Microsoft.AspNetCore.Mvc;
 using PM.Identity.Application.Interfaces;
-using PM.Shared.Dtos.Users;
 
 namespace PM.Identity.API.Controllers
 {
@@ -15,48 +13,6 @@ namespace PM.Identity.API.Controllers
         {
             _userService = userService;
             _logger = logger;
-        }
-        [HttpGet("get-user-by-email")]
-        public async Task<IActionResult> GetUserByEmail(string email)
-        {
-            _logger.LogInformation("GetUserByEmail request received for email: {Email}", email);
-            var result = await _userService.GetUserByEmail(email);
-            if (result.Status)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return BadRequest(result);
-            }
-        }
-        [HttpGet("get-user-by-id")]
-        public async Task<IActionResult> GetUserById(string id)
-        {
-            _logger.LogInformation("GetUserById request received for id: {Id}", id);
-            var result = await _userService.GetUserById(id);
-            if (result.Status)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return BadRequest(result);
-            }
-        }
-        [HttpPatch("update-user")]
-        public async Task<IActionResult> UpdateUser(UpdateUserModel model)
-        {
-            _logger.LogInformation("UpdateUser request received for id: {Id}", model.Id);
-            var result = await _userService.UpdateUser(model.Id, model.FirstName, model.LastName, model.UserName, model.AvatarPath);
-            if (result.Status)
-            {
-                return Ok(result);
-            }
-            else
-            {
-                return BadRequest(result);
-            }
         }
     }
 }
