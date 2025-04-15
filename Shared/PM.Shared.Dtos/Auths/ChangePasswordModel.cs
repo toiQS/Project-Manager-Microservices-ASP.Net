d5694fe7c476f56e6fbf3567f5dc886ec7546da4
Akai <nguyensieu12112002@gmail.com>
2025-04-15 18:07:45 +0700

    Delete ChangePasswordModel.cs

--- Shared/PM.Shared.Dtos/Auths/ChangePasswordModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PM.Shared.Dtos.Auths
-{
-    public class ChangePasswordModel
-    {
-        public string Email { get; set; } = string.Empty;
-        public string OldPassword { get; set; } = string.Empty;
-        public string NewPassword { get; set; } = string.Empty;
-    }
-}
