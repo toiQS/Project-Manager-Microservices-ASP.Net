ec5b094baec0c30696cfd25afbccbbd1cdbc9b2c
Akai <nguyensieu12112002@gmail.com>
2025-04-15 18:07:42 +0700

    Create ProjectDbContext.cs

---
+++ Services/project-service/PM.Project.Infrastructure/ProjectDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace PM.Project.Infrastructure
+{
+    public class ProjectDbContext : DbContext
+    {
+        public ProjectDbContext(DbContextOptions<ProjectDbContext> options) : base(options)
+        {
+        }
+    }
+}
