5ca5593f0dba9eb922aa1a47bcceae4f84af4c70
Akai <nguyensieu12112002@gmail.com>
2025-04-15 18:07:53 +0700

    Delete RegisterModel.cs

--- Shared/PM.Shared.Dtos/Auths/RegisterModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace PM.Shared.Dtos.Auths
-{
-    public class RegisterModel
-    {
-        public string Email { get; set; } = string.Empty;
-        public string UserName { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
