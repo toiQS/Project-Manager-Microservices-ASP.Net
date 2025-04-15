2759c01056ee185f9b03d84ad97cf00604ec70b5
Akai <nguyensieu12112002@gmail.com>
2025-04-15 18:08:32 +0700

    Update Program.cs

--- EndPoints/PM.EndPoint.API/Program.cs
+++ EndPoints/PM.EndPoint.API/Program.cs
@@ -1,44 +1,18 @@
-using Microsoft.OpenApi.Models;
 using PM.EndPoint.API;
 
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.InitializeFlow(builder.Configuration);
+
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
 
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddOpenApiDocument();
+
 builder.Services.AddHttpContextAccessor();
 //builder.Services.AddSwaggerGen();
 
-builder.Services.AddSwaggerGen(c =>
-{
-    c.SwaggerDoc("v1", new OpenApiInfo { Title = "PM.Identity API", Version = "v1" });
-
-    var securityScheme = new OpenApiSecurityScheme
-    {
-        Name = "Authorization",
-        Type = SecuritySchemeType.Http,
-        Scheme = "bearer",
-        BearerFormat = "JWT",
-        In = ParameterLocation.Header,
-        Description = "JWT Authorization header using the Bearer scheme.",
-        Reference = new OpenApiReference
-        {
-            Type = ReferenceType.SecurityScheme,
-            Id = "Bearer"
-        }
-    };
-
-    c.AddSecurityDefinition("Bearer", securityScheme);
-    c.AddSecurityRequirement(new OpenApiSecurityRequirement
-    {
-        { securityScheme, Array.Empty<string>() }
-    });
-});
 
 
 var app = builder.Build();
@@ -47,8 +21,6 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
-    app.UseSwagger();
-    app.UseSwaggerUi();
 }
 
 
