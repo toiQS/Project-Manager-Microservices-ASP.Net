04366cb65e58f97a6ef81dfe0d0a97a6103fb7b0
Akai <nguyensieu12112002@gmail.com>
2025-04-15 18:08:26 +0700

    Update Program.cs

--- gateway/PM.Gateway.API/Program.cs
+++ gateway/PM.Gateway.API/Program.cs
@@ -1,32 +1,21 @@
-using Microsoft.IdentityModel.Tokens;
 using Ocelot.DependencyInjection;
 using Ocelot.Middleware;
 
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Configuration.AddJsonFile("Ocelot/IdentityOcelot.json", optional: false, reloadOnChange: true);
-//builder.Configuration.AddJsonFile("Ocelot/UserOcelot.json", optional: false, reloadOnChange: true);
+
 
 builder.Services.AddOcelot(builder.Configuration);
 builder.Services.AddControllers();
-builder.Services.AddAuthentication("Bearer")
-    .AddJwtBearer("Bearer", options =>
-    {
-        options.Authority = "https://localhost:7130";
-        options.RequireHttpsMetadata = true;
-        options.TokenValidationParameters = new TokenValidationParameters
-        {
-            ValidateAudience = false
-        };
-    });
-
 
 var app = builder.Build();
 
 app.UseHttpsRedirection();
+
+//app.UseAuthentication();
 app.UseAuthorization();
-app.UseAuthentication();
-app.UseAuthorization();
+
 app.MapControllers();
 await app.UseOcelot();
 
