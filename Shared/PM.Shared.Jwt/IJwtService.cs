794a12b2e6151fb034c827a8ed66176181469004
Akai <nguyensieu12112002@gmail.com>
2025-04-19 06:20:11 +0700

    Create IJwtService.cs

---
+++ Shared/PM.Shared.Jwt/IJwtService.cs
@@ -0,0 +1,9 @@
+﻿using PM.Shared.Dtos;
+
+namespace PM.Shared.Jwt
+{
+    public interface IJwtService
+    {
+        public ServiceResult<string> GenerateToken(string userId, string email, string role);
+    }
+}
