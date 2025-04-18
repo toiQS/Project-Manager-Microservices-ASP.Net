e8b6c108eb0796259c9631372302e2244ef4da9e
Akai <nguyensieu12112002@gmail.com>
2025-04-18 10:52:42 +0700

    Create SetupSwaggerIdentity.cs

---
+++ Shared/PM.Shared.Swagger/SetupSwaggerIdentity.cs
@@ -0,0 +1,37 @@
+﻿using OpenApiSecurityRequirement = Microsoft.OpenApi.Models.OpenApiSecurityRequirement;
+using OpenApiSecurityScheme = Microsoft.OpenApi.Models.OpenApiSecurityScheme;
+
+namespace PM.Shared.Swagger
+{
+    public static class SetupSwaggerIdentity
+    {
+        public static void InitializeSwaggerInitalize(this IServiceCollection services, IConfiguration configuration, string title)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = title, Version = "v1" });
+                c.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo { Title = title, Version = "v2" });
+                c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
+                {
+                    Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
+                    Name = "Authorization",
+                    In = ParameterLocation.Header,
+                    Type = SecuritySchemeType.Http,
+                    Scheme = "bearer",
+                    BearerFormat = "JWT"
+                });
+                c.AddSecurityRequirement(new OpenApiSecurityRequirement
+                {
+                    { new OpenApiSecurityScheme
+                        {
+                            Reference = new OpenApiReference
+                            {
+                                Type = ReferenceType.SecurityScheme,
+                                Id = "Bearer"
+                            }
+                        }, new string[] { } }
+                });
+            });
+        }
+    }
+}
