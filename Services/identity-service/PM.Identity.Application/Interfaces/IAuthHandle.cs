e2032897158df1e59ac2170392e2503c2579fee4
Akai <nguyensieu12112002@gmail.com>
2025-04-19 06:21:14 +0700

    Create IAuthHandle.cs

---
+++ Services/identity-service/PM.Identity.Application/Interfaces/IAuthHandle.cs
@@ -0,0 +1,11 @@
+﻿using PM.Shared.Dtos;
+using System.Data;
+
+namespace PM.Identity.Application.Interfaces
+{
+    public interface IAuthHandle
+    {
+        public Task<ServiceResult<string>> RegisterHandleAsync(RegisterModel model);
+        public Task<ServiceResult<string>> LoginHandleAsync(LoginModel model);
+    }
+}
