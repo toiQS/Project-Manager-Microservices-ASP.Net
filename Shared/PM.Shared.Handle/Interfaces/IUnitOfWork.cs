eb4074db2f7105ff19ca88fe04aa5b8a22c5fc9b
Akai <nguyensieu12112002@gmail.com>
2025-04-18 10:51:52 +0700

    Create IUnitOfWork.cs

---
+++ Shared/PM.Shared.Handle/Interfaces/IUnitOfWork.cs
@@ -0,0 +1,10 @@
+﻿using PM.Shared.Dtos;
+
+namespace PM.Shared.Handle.Interfaces
+{
+    public interface IUnitOfWork<TData> where TData : DbContext
+    {
+        
+        public Task<ServiceResult<bool>> ExecuteTransactionAsync(Func<Task> transactionOperations, CancellationToken cancellationToken = default);
+    }
+}
