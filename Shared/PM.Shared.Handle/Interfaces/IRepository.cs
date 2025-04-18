d8ff46451dc75f973ac30745c680728c1b15dfa8
Akai <nguyensieu12112002@gmail.com>
2025-04-18 10:51:50 +0700

    Create IRepository.cs

---
+++ Shared/PM.Shared.Handle/Interfaces/IRepository.cs
@@ -0,0 +1,17 @@
+﻿using PM.Shared.Dtos;
+
+namespace PM.Shared.Handle.Interfaces
+{
+    public interface IRepository<TData, TEntity> where TData : DbContext where TEntity : class
+    {
+        public Task<ServiceResult<IEnumerable<TEntity>>> GetAllAsync();
+        public Task<ServiceResult<IEnumerable<TEntity>>> GetManyAsync(string key, string valueKey);
+        public Task<ServiceResult<TEntity>> GetOneAsync(string key, string valueKey);
+        public Task<ServiceResult<bool>> IsExistName(IEnumerable<TEntity> arr, string name);
+        public Task AddAsync(TEntity entity);
+        public Task UpdateAsync(TEntity entity);
+        public Task PacthAsync(TEntity entity, Dictionary<string, object> keyValuePairs);
+        public Task DeleteAsync(TEntity entity);
+    }
+
+}
