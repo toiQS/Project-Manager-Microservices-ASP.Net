2da8d80ed95e98ef1798debfd97f0c4610dc5247
Akai <nguyensieu12112002@gmail.com>
2025-04-19 06:20:10 +0700

    Create Config.cs

---
+++ Shared/PM.Shared.Jwt/Config.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace PM.Shared.Jwt
+{
+    public static class Config
+    {
+        public static void InitializeJwt(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.AddAuthentication(options =>
+            {
+                options.DefaultAuthenticateScheme = JwtBearerDefaults.AuthenticationScheme;
+                options.DefaultChallengeScheme = JwtBearerDefaults.AuthenticationScheme;
+
+            }).AddJwtBearer(options =>
+            {
+                options.TokenValidationParameters = new Microsoft.IdentityModel.Tokens.TokenValidationParameters
+                {
+                    ValidateIssuer = true,
+                    ValidateAudience = true,
+                    ValidateLifetime = true,
+                    ValidateIssuerSigningKey = true,
+                    ValidIssuer = configuration.GetSection("Jwt:Issuer").Value,
+                    ValidAudience = configuration.GetSection("Jwt:Audience").Value,
+                    IssuerSigningKey = new Microsoft.IdentityModel.Tokens.SymmetricSecurityKey(System.Text.Encoding.UTF8.GetBytes(configuration.GetSection("Jwt:SecretKey").Value))
+                };
+            });
+        }
+    }
+}
