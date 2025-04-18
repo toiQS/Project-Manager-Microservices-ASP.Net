5debd55690eeb48ff010aafccb5d1207e0a92aea
Akai <nguyensieu12112002@gmail.com>
2025-04-18 10:51:38 +0700

    Create ConfigService.cs

---
+++ Shared/PM.Shared.Config/ConfigService.cs
@@ -0,0 +1,25 @@
+﻿namespace PM.Shared.Config
+{
+    public static class ConfigService
+
+    {
+        public static void Initialize<TContext>(IServiceCollection services, IConfiguration configuration) where TContext : DbContext
+        {
+            services.InitializeSQL<TContext>(configuration);
+        }
+        public static void InitializeSQL<TContext>(this IServiceCollection services, IConfiguration configuration) where TContext : DbContext
+        {
+            services.AddDbContext<TContext>(options =>
+            {
+                options.UseSqlServer(configuration.GetConnectionString("DefaultConnection"));
+            });
+        }
+
+    }
+
+    // Move the extension method to a non-generic static class
+    public static class ServiceCollectionExtensions
+    {
+       
+    }
+}
