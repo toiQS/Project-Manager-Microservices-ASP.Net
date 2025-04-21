79bad3fa2dbb9e43fffd1f62a842502ffa9c59e9
Akai <nguyensieu12112002@gmail.com>
2025-04-21 22:15:30 +0700

    Update Program.cs

--- Web/PM.Web.API/Program.cs
+++ Web/PM.Web.API/Program.cs
@@ -1,10 +1,16 @@
+using PM.Shared.Swagger;
 var builder = WebApplication.CreateBuilder(args);
+builder.Services.InitializeSwagger(
+    builder.Configuration,
+    "PM.Web.API"
+);
 
 // Add services to the container.
 
 builder.Services.AddControllers();
 // Learn more about configuring OpenAPI at https://aka.ms/aspnet/openapi
 builder.Services.AddOpenApi();
+builder.Services.AddHttpContextAccessor();
 
 var app = builder.Build();
 
@@ -12,6 +18,18 @@ var app = builder.Build();
 if (app.Environment.IsDevelopment())
 {
     app.MapOpenApi();
+    //app.MapSwagger();
+    app.UseSwagger(options =>
+    {
+        // Configure SwaggerOptions explicitly to resolve ambiguity
+    });
+    app.UseSwaggerUI(c =>
+    {
+        c.SwaggerEndpoint("/swagger/v1/swagger.json", "PM.Web.API v1");
+        c.SwaggerEndpoint("/swagger/v2/swagger.json", "PM.Web.API v2");
+        c.RoutePrefix = string.Empty; // Set Swagger UI at the app's root
+    });
+   
 }
 
 app.UseHttpsRedirection();
