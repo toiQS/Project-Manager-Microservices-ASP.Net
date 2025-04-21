d8362f82f72999eed29b951d4ae12a7a3899f296
Akai <nguyensieu12112002@gmail.com>
2025-04-21 22:15:15 +0700

    Update ValuesController.cs

--- Services/identity-service/PM.Identity.API/Controllers/ValuesController.cs
+++ Services/identity-service/PM.Identity.API/Controllers/ValuesController.cs
@@ -1,6 +1,7 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace PM.Identity.API.Controllers
 {
@@ -9,7 +10,27 @@ namespace PM.Identity.API.Controllers
     [Authorize]
     public class ValuesController : ControllerBase
     {
-        public ValuesController() { }
+        private readonly IHttpContextAccessor _httpContextAccessor;
+        private readonly HttpClient _httpClient;
+
+        public ValuesController(IHttpContextAccessor httpContextAccessor)
+        {
+            _httpContextAccessor = httpContextAccessor;
+            _httpClient = new HttpClient();
+        }
+        [HttpGet("demo-2")]
+        public IActionResult demo2()
+        {
+            var user = _httpContextAccessor.HttpContext.User;
+            if(user.Identity.IsAuthenticated)
+            {
+                return Ok(new { message = "Hello from Identity API" });
+            }
+            else
+            {
+                return Unauthorized();
+            }
+        }
         [HttpGet("demo")]
         public IActionResult Get()
         {
