939ce47a50d8bb563558224a2b02fe2993c496ce
Akai <nguyensieu12112002@gmail.com>
2025-04-22 18:38:51 +0700

    Update SetupSwaggerIdentity.cs

--- Shared/PM.Shared.Swagger/SetupSwaggerIdentity.cs
+++ Shared/PM.Shared.Swagger/SetupSwaggerIdentity.cs
@@ -12,8 +12,7 @@ namespace PM.Shared.Swagger
         {
             services.AddSwaggerGen(c =>
             {
-                c.SwaggerDoc("v1", new Microsoft.OpenApi.Models.OpenApiInfo { Title = title, Version = "v1" });
-                c.SwaggerDoc("v2", new Microsoft.OpenApi.Models.OpenApiInfo { Title = title, Version = "v2" });
+                c.SwaggerDoc("v1", new OpenApiInfo { Title = title, Version = "v1" });
                 c.AddSecurityDefinition("Bearer", new OpenApiSecurityScheme
                 {
                     Description = "JWT Authorization header using the Bearer scheme. \r\n\r\n Enter 'Bearer' [space] and then your token in the text input below.\r\n\r\nExample: \"Bearer 12345abcdef\"",
@@ -24,16 +23,16 @@ namespace PM.Shared.Swagger
                     BearerFormat = "JWT"
                 });
                 c.AddSecurityRequirement(new OpenApiSecurityRequirement
+    {
+        { new OpenApiSecurityScheme
+            {
+                Reference = new OpenApiReference
                 {
-                    { new OpenApiSecurityScheme
-                        {
-                            Reference = new OpenApiReference
-                            {
-                                Type = ReferenceType.SecurityScheme,
-                                Id = "Bearer"
-                            }
-                        }, new string[] { } }
-                });
+                    Type = ReferenceType.SecurityScheme,
+                    Id = "Bearer"
+                }
+            }, new string[] { } }
+    });
             });
         }
     }
