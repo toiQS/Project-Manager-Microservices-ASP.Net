ff5fb7e04841248ce66a8d3c30eccad183c6c125
Akai <nguyensieu12112002@gmail.com>
2025-04-23 23:48:28 +0700

    Update Program.cs

--- Hosting/PM.Hosting/Program.cs
+++ Hosting/PM.Hosting/Program.cs
@@ -4,4 +4,5 @@ builder.AddProject<Projects.PM_Identity_API>("pm-identity-api");
 builder.AddProject<Projects.PM_Web_API>("pm-web-api");
 
 builder.AddProject<Projects.PM_Gateway>("pm-gateway");
+builder.AddProject<Projects.PM_Tracking_API>("pm-tracking-api");
 builder.Build().Run();
