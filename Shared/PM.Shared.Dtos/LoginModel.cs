d17b2d727b4c989e36e4632c63398cc480c325e1
Akai <nguyensieu12112002@gmail.com>
2025-04-23 23:15:38 +0700

    Delete LoginModel.cs

--- Shared/PM.Shared.Dtos/LoginModel.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace PM.Shared.Dtos
-{
-    public class LoginModel
-    {
-        public string Email { get; set; } = string.Empty;
-        public string Password { get; set; } = string.Empty;
-    }
-}
