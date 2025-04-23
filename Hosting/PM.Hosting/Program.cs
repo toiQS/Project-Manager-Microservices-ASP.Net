4a0183faaeb4ea13530deb22934675cecc5ee536
Akai <nguyensieu12112002@gmail.com>
2025-04-23 23:14:36 +0700

    Create Program.cs

---
+++ Hosting/PM.Hosting/Program.cs
@@ -0,0 +1,7 @@
+var builder = DistributedApplication.CreateBuilder(args);
+builder.AddProject<Projects.PM_Identity_API>("pm-identity-api");
+
+builder.AddProject<Projects.PM_Web_API>("pm-web-api");
+
+builder.AddProject<Projects.PM_Gateway>("pm-gateway");
+builder.Build().Run();
