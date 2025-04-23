1a17989aabc46a20d33972e2d1150d45b4353cb5
Akai <nguyensieu12112002@gmail.com>
2025-04-23 23:15:20 +0700

    Create TrackingDbContext.cs

---
+++ Services/tracking-service/PM.Tracking.Infrastructure/TrackingDbContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+using PM.Tracking.Domain;
+
+namespace PM.Tracking.Infrastructure
+{
+    public class TrackingDbContext : DbContext
+    {
+        public TrackingDbContext(DbContextOptions<TrackingDbContext> options) : base(options) { }
+        public DbSet<ActivityLog> ActivityLog { get; set; }
+    }
+}
