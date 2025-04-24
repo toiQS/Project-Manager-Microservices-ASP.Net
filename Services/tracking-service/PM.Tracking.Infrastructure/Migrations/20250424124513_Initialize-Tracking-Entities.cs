564d2e2fcd706fd08059d2fa36522c01fd464f5a
Akai <nguyensieu12112002@gmail.com>
2025-04-24 20:30:53 +0700

    Create 20250424124513_Initialize-Tracking-Entities.cs

---
+++ Services/tracking-service/PM.Tracking.Infrastructure/Migrations/20250424124513_Initialize-Tracking-Entities.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PM.Tracking.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeTrackingEntities : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "ActivityLogs",
+                columns: table => new
+                {
+                    Id = table.Column<string>(type: "nvarchar(450)", nullable: false),
+                    UserId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ProjectId = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ActionName = table.Column<string>(type: "nvarchar(max)", nullable: false),
+                    ActionAt = table.Column<DateTime>(type: "datetime2", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_ActivityLogs", x => x.Id);
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "ActivityLogs");
+        }
+    }
+}
