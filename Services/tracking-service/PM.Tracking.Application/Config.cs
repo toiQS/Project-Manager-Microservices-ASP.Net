3b45054878504c16b5d9f29a5e1f89c149bf0966
Akai <nguyensieu12112002@gmail.com>
2025-04-24 20:31:13 +0700

    Create Config.cs

---
+++ Services/tracking-service/PM.Tracking.Application/Config.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using PM.Shared.Config;
+using PM.Tracking.Infrastructure.Data;
+
+namespace PM.Tracking.Application
+{
+    public static class Config
+    {
+        public static void InitializeApplication(this IServiceCollection services, IConfiguration configuration)
+        {
+            services.InitializeSQL<TrackingDbContext>(configuration, "TrackingConnectString");
+        }
+    }
+}
