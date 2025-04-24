370e8ede21fcfa5c2e580f406921c61508133ddb
Akai <nguyensieu12112002@gmail.com>
2025-04-24 20:30:47 +0700

    Create AddTrackingModel.cs

---
+++ Shared/PM.Shared.Dtos/tracking/AddTrackingModel.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.Shared.Dtos.tracking
+{
+    public class AddTrackingModel
+    {
+        public string UserId { get; set; } = string.Empty;
+        public string ProjectId { get; set; } = string.Empty;
+        public string ActionName { get; set; } = string.Empty;
+    }
+}
