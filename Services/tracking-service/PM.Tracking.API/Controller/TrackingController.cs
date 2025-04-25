f56f350e10e7adee28be16fe60db365514577b1e
Akai <nguyensieu12112002@gmail.com>
2025-04-25 11:52:15 +0700

    Update TrackingController.cs

--- Services/tracking-service/PM.Tracking.API/Controller/TrackingController.cs
+++ Services/tracking-service/PM.Tracking.API/Controller/TrackingController.cs
@@ -1,5 +1,8 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using PM.Shared.Dtos.auths;
+using PM.Shared.Dtos.tracking;
+using PM.Tracking.Application.Interfaces;
 
 namespace PM.Tracking.API.Controller
 {
@@ -8,6 +11,24 @@ namespace PM.Tracking.API.Controller
     public class TrackingController : ControllerBase
     {
 
-
+        private readonly ITrackingHandle _trackingHandle;
+        private readonly ILogger<TrackingController> _logger;
+        public TrackingController(ITrackingHandle trackingHandle, ILogger<TrackingController> logger)
+        {
+            _trackingHandle = trackingHandle;
+            _logger = logger;
+        }
+        [HttpPost("add-tracking-log")]
+        public async Task<IActionResult> AddAsync(AddTrackingModel model)
+        {
+            _logger.LogInformation("Adding tracking activity log");
+            var result = await _trackingHandle.AddHandle(model);
+            if (result.Status != ResultStatus.Success)
+            {
+                _logger.LogError("Error when to do action");
+                return BadRequest(result);
+            }
+            return Ok(result);
+        }
     }
 }
