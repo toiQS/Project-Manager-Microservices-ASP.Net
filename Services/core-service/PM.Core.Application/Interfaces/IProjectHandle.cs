fc4d308fb29d007a6fcd385db901ec421d54ab0c
Akai <nguyensieu12112002@gmail.com>
2025-04-28 21:28:54 +0700

    Create IProjectHandle.cs

---
+++ Services/core-service/PM.Core.Application/Interfaces/IProjectHandle.cs
@@ -0,0 +1,10 @@
+﻿using PM.Shared.Dtos.auths;
+using PM.Shared.Dtos.cores.projects;
+
+namespace PM.Core.Application.Interfaces
+{
+    public interface IProjectHandle
+    {
+        Task<ServiceResult<IEnumerable<IndexProjectModel>>> ProjectsUserHasJoined(string userId);
+    }
+}
