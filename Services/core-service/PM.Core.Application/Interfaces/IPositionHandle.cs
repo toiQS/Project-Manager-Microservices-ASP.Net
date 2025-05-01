06f86c1a32d575bf5d5fd2a838e269f9c2fcc0d3
Akai <nguyensieu12112002@gmail.com>
2025-05-01 17:14:31 +0700

    Create IPositionHandle.cs

---
+++ Services/core-service/PM.Core.Application/Interfaces/IPositionHandle.cs
@@ -0,0 +1,15 @@
+﻿using PM.Core.Entities;
+using PM.Shared.Dtos.auths;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.Core.Application.Interfaces
+{
+    public interface IPositionHandle
+    {
+        Task<ServiceResult<Position>> GetPositionByName(string name);
+    }
+}
