572a309de13e0f8198a41d5e744e1c3eed567144
Akai <nguyensieu12112002@gmail.com>
2025-05-01 17:14:34 +0700

    Update AddProjectModel.cs

--- Shared/PM.Shared.Dtos/cores/projects/AddProjectModel.cs
+++ Shared/PM.Shared.Dtos/cores/projects/AddProjectModel.cs
@@ -4,12 +4,7 @@
     {
         public string Name { get; set; } = string.Empty;
         public string Description { get; set; } = string.Empty;
-        public DateTime StartAt { get; set; }
-        public DateTime EndAt { get; set; }
-        public TypeStatus Status { get; set; }
-        public bool IsDeleted { get; set; }
-        public bool IsModified { get; set; }
-        public bool IsLocked { get; set; }
-        public bool IsComplied { get; set; }
+        public DateOnly StartAt { get; set; }
+        public DateOnly EndAt { get; set; }
     }
 }
