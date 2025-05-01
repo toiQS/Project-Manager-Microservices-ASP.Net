a3a3a6594fe1d981a735586d9f2297974a6390a8
Akai <nguyensieu12112002@gmail.com>
2025-05-01 17:14:27 +0700

    Create PositionHandle.cs

---
+++ Services/core-service/PM.Core.Application/Implements/PositionHandle.cs
@@ -0,0 +1,38 @@
+﻿using PM.Core.Entities;
+using PM.Core.Infrastructure.Data;
+using PM.Shared.Dtos.auths;
+using PM.Shared.Handle.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.Core.Application.Implements
+{
+    public class PositionHandle
+    {
+        private readonly IUnitOfWork<CoreDbContext> _unitOfWork;
+        public async Task<ServiceResult<Position>> GetPositionByName(string name)
+        {
+            if (string.IsNullOrEmpty(name))
+            {
+                return ServiceResult<Position>.Error("");
+            }
+            try
+            {
+                var position = await _unitOfWork.Repository<Position>().GetOneAsync("Name", name);
+                if (position.Status != ResultStatus.Success || position.Data == null)
+                {
+                    return ServiceResult<Position>.Error("");
+                }
+
+                return ServiceResult<Position>.Success(position.Data);
+            }
+            catch (Exception ex)
+            {
+                return ServiceResult<Position>.FromException(ex);
+            }
+        }
+    }
+}
