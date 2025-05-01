56f8b4d76a822a2fbc2e9b67a2a943e810033f68
Akai <nguyensieu12112002@gmail.com>
2025-05-01 17:14:22 +0700

    Create StatusHandle.cs

---
+++ Shared/PM.Shared.Handle/Implements/StatusHandle.cs
@@ -0,0 +1,36 @@
+﻿using PM.Shared.Dtos.cores;
+using PM.Shared.Handle.Interfaces;
+
+namespace PM.Shared.Handle.Implements
+{
+    public class StatusHandle
+    {
+        private DateTime _startAt;
+        private DateTime _endAt;
+        private bool _isCompleted;
+        public StatusHandle(DateTime startAt, DateTime endAt, bool isCompleted)
+        {
+            _endAt = endAt;
+            _startAt = startAt;
+            _isCompleted = isCompleted;
+        }
+        public int GetStatus()
+        {
+            if(_startAt == _endAt) return (int) TypeStatus.NotSelected;
+            if (DateTime.Now < _startAt) return (int)TypeStatus.Waiting;
+            if (DateTime.Now >= _startAt && DateTime.Now < _endAt) return (int)TypeStatus.InProgress;
+            if (!_isCompleted)
+            {
+                
+                if (_endAt > DateTime.Now) return (int)TypeStatus.BehindSchedule;
+            }
+            else
+            {
+                if(_endAt < DateTime.Now) return (int) TypeStatus.CompletedEarly;
+                if(_endAt > DateTime.Now) return (int) TypeStatus.FinishedLate;
+                if (_endAt == DateTime.Now) return (int)TypeStatus.FinishedOnTime;
+            }
+            return (int)TypeStatus.Node;
+        }
+    }
+}
