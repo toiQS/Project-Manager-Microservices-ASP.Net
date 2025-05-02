b9048669be1618d0093f218f8d569a28c0d8d96e
Akai <nguyensieu12112002@gmail.com>
2025-05-02 11:42:25 +0700

    Create PacthPlanModel.cs

---
+++ Shared/PM.Shared.Dtos/cores/plans/PacthPlanModel.cs
@@ -0,0 +1,10 @@
+﻿namespace PM.Shared.Dtos.cores.plans
+{
+    public class PacthPlanModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Goal { get; set; } = string.Empty;
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+    }
+}
