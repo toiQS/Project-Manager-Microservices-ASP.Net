4bad83f390e9fab0cff0eefa614dbbc6165cc167
Akai <nguyensieu12112002@gmail.com>
2025-05-02 09:30:06 +0700

    Update CoreDbContext.cs

--- Services/core-service/PM.Core.Infrastructure/Data/CoreDbContext.cs
+++ Services/core-service/PM.Core.Infrastructure/Data/CoreDbContext.cs
@@ -15,5 +15,34 @@ namespace PM.Core.Infrastructure.Data
         public DbSet<Position> Positions { get; set; }
         public DbSet<Project> Projects { get; set; }
         public DbSet<ProjectMember> Members { get; set; }
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<Plan>()
+                .HasOne(p => p.ProjectMember)
+                .WithMany()
+                .HasForeignKey(p => p.ProjectMemberId)
+                .OnDelete(DeleteBehavior.Restrict); // hoặc .NoAction()
+
+            modelBuilder.Entity<MissionMember>()
+                .HasOne(mm => mm.ProjectMember)
+                .WithMany()
+                .HasForeignKey(mm => mm.MemberId)
+                .OnDelete(DeleteBehavior.Restrict); // hoặc .NoAction()
+
+            modelBuilder.Entity<ProjectMember>()
+                .HasOne(pm => pm.Project)
+                .WithMany()
+                .HasForeignKey(pm => pm.ProjectId)
+                .OnDelete(DeleteBehavior.Restrict);
+
+            modelBuilder.Entity<ProjectMember>()
+                .HasOne(pm => pm.Position)
+                .WithMany()
+                .HasForeignKey(pm => pm.PositionId)
+                .OnDelete(DeleteBehavior.Restrict);
+        }
+
     }
 }
