cd7c88c7a0861b9cfebe00ae4ed60b5424d8ee23
Akai <nguyensieu12112002@gmail.com>
2025-05-02 16:24:46 +0700

    Create 20250502092257_Initialize-Core-Entities-2.cs

---
+++ Services/core-service/PM.Core.Infrastructure/Migrations/20250502092257_Initialize-Core-Entities-2.cs
@@ -0,0 +1,82 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace PM.Core.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class InitializeCoreEntities2 : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "PlanId",
+                table: "Missions",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.AddColumn<string>(
+                name: "ProjectMemberId",
+                table: "Missions",
+                type: "nvarchar(450)",
+                nullable: false,
+                defaultValue: "");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Missions_PlanId",
+                table: "Missions",
+                column: "PlanId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Missions_ProjectMemberId",
+                table: "Missions",
+                column: "ProjectMemberId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Missions_Members_ProjectMemberId",
+                table: "Missions",
+                column: "ProjectMemberId",
+                principalTable: "Members",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Missions_Plans_PlanId",
+                table: "Missions",
+                column: "PlanId",
+                principalTable: "Plans",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Missions_Members_ProjectMemberId",
+                table: "Missions");
+
+            migrationBuilder.DropForeignKey(
+                name: "FK_Missions_Plans_PlanId",
+                table: "Missions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Missions_PlanId",
+                table: "Missions");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Missions_ProjectMemberId",
+                table: "Missions");
+
+            migrationBuilder.DropColumn(
+                name: "PlanId",
+                table: "Missions");
+
+            migrationBuilder.DropColumn(
+                name: "ProjectMemberId",
+                table: "Missions");
+        }
+    }
+}
