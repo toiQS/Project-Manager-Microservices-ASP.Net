39dbc7aa7d46d4af0cb9ab4485415708ebe25907
Akai <nguyensieu12112002@gmail.com>
2025-05-02 18:44:14 +0700

    Update Mission.cs

--- Services/core-service/PM.Core.Entities/Mission.cs
+++ Services/core-service/PM.Core.Entities/Mission.cs
@@ -12,7 +12,7 @@ namespace PM.Core.Entities
         public string PlanId {  get; set; } = string.Empty;
         public string ProjectMemberId {  get; set; } = string.Empty;
         public TypeStatus Status { get; set; }
-        public string CreateBy { get; set; } = string.Empty;
+        //public string CreateBy { get; set; } = string.Empty;
         public DateTime CreateDate { get; set; }
         public DateTime StartDate { get; set; }
         public DateTime EndDate { get; set; }
