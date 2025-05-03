6b1bd15aa9d39fadfaad96184ca1b8e19f7bc94b
Akai <nguyensieu12112002@gmail.com>
2025-05-03 23:31:27 +0700

    Create PatchMissionModel.cs

---
+++ Shared/PM.Shared.Dtos/cores/missions/PatchMissionModel.cs
@@ -0,0 +1,11 @@
+﻿namespace PM.Shared.Dtos.cores.missions
+{
+    public class PatchMissionModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Request {  get; set; } = string.Empty;
+        //public string PlanId { get; set; } = string.Empty;
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
