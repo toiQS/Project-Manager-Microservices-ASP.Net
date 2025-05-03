c0babd57e2a3ad243cced8556005f757cdb8fa3d
Akai <nguyensieu12112002@gmail.com>
2025-05-03 23:31:23 +0700

    Create DetailMissionModel.cs

---
+++ Shared/PM.Shared.Dtos/cores/missions/DetailMissionModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PM.Shared.Dtos.cores.missions
+{
+    public class DetailMissionModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Goal { get; set; } = string.Empty;
+        public string CreateBy { get; set; } = string.Empty;
+        public DateTime CreateDate { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+    }
+}
