a013374edb50cfd409fe6e353c25cf51b22f7631
Akai <nguyensieu12112002@gmail.com>
2025-05-03 23:31:18 +0700

    Create IMissionHandle.cs

---
+++ Services/core-service/PM.Core.Application/Interfaces/IMissionHandle.cs
@@ -0,0 +1,15 @@
+﻿using PM.Shared.Dtos.auths;
+using PM.Shared.Dtos.cores.missions;
+
+namespace PM.Core.Application.Interfaces
+{
+    public interface IMissionHandle
+    {
+        Task<ServiceResult<IEnumerable<IndexMissionModel>>> GetAsync(string planId);
+        Task<ServiceResult<DetailMissionModel>> GetDetailAsync(string missionId);
+        Task<ServiceResult<DetailMissionModel>> AddAsync(string userId, AddMissonModel model);
+        Task<ServiceResult<DetailMissionModel>> PatchMissionAsync(string userId, string missionId, PatchMissionModel model);
+        Task<ServiceResult<IEnumerable<IndexMissionModel>>> DeleteAsync(string userId, string missionId);
+        Task<ServiceResult<IEnumerable<IndexMissionModel>>> DeleteManyAsync(string userId, string planId);
+    }
+}
