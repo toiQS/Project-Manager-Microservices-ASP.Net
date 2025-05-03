67c2782b246222ffcf8e1e4f15bc7069bf5ff543
Akai <nguyensieu12112002@gmail.com>
2025-05-03 23:31:25 +0700

    Create IndexMissionModel.cs

---
+++ Shared/PM.Shared.Dtos/cores/missions/IndexMissionModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Shared.Dtos.cores.missions
+{
+    public class IndexMissionModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string Name { get; set; } = string.Empty;
+        public string Request {  get; set; } = string.Empty;
+    }
+}
