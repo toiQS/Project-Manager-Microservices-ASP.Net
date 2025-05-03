12b22ce0e2c7c28410a0e5a4ed498070d3e8a341
Akai <nguyensieu12112002@gmail.com>
2025-05-03 23:31:21 +0700

    Create AddMissonModel.cs

---
+++ Shared/PM.Shared.Dtos/cores/missions/AddMissonModel.cs
@@ -0,0 +1,13 @@
+﻿namespace PM.Shared.Dtos.cores.missions
+{
+    public class AddMissonModel
+    {
+        public string Name { get; set; } = string.Empty;
+        public string Request { get; set; } = string.Empty;
+        public string PlanId { get; set; } = string.Empty;
+        public string ProjectMemberId { get; set; } = string.Empty;
+        public TypeStatus Status { get; set; }
+        public DateOnly StartDate { get; set; }
+        public DateOnly EndDate { get; set; }
+    }
+}
