972f72bcce3b71822d2aa7f44ad5d2fcbb0b074a
Akai <nguyensieu12112002@gmail.com>
2025-05-03 10:25:10 +0700

    Create MissionHandle.cs

---
+++ Services/core-service/PM.Core.Application/Implements/MissionHandle.cs
@@ -0,0 +1,17 @@
+﻿using PM.Core.Infrastructure.Data;
+using PM.Shared.Dtos.users;
+using PM.Shared.Handle.Interfaces;
+
+namespace PM.Core.Application.Implements
+{
+    public class MissionHandle
+    {
+        private readonly IUnitOfWork<CoreDbContext> _unitOfWork;
+        private readonly IAPIService<UserDetail> _userAPI;
+        public MissionHandle(IUnitOfWork<CoreDbContext> unitOfWork, IAPIService<UserDetail> userAPI)
+        {
+            _unitOfWork = unitOfWork;
+            _userAPI = userAPI;
+        }
+    }
+}
