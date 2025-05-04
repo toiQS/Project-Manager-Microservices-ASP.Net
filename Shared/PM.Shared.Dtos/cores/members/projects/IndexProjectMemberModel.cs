3f53873e62b4aff1c36b28762ad68482fcf6770e
Akai <nguyensieu12112002@gmail.com>
2025-05-05 01:15:29 +0700

    Create IndexProjectMemberModel.cs

---
+++ Shared/PM.Shared.Dtos/cores/members/projects/IndexProjectMemberModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Shared.Dtos.cores.members.projects
+{
+    public class IndexProjectMemberModel
+    {
+        public string Id { get; set; } = string.Empty;
+        public string UserName { get; set; } = string.Empty;
+        public string PositionName {  get; set; } = string.Empty;
+    }
+}
