c6c057779402e31e2f6d3a8689fbef930aba2f70
Akai <nguyensieu12112002@gmail.com>
2025-05-05 01:15:31 +0700

    Create PacthProjectMemberModel.cs

---
+++ Shared/PM.Shared.Dtos/cores/members/projects/PacthProjectMemberModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PM.Shared.Dtos.cores.members.projects
+{
+    public class PacthProjectMemberModel
+    {
+        public string PositionId { get; set; } = string.Empty;
+        public string UserId { get; set; } = string.Empty;
+    }
+}
