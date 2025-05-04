2d481cb162aafa6d5e9574158aa48bf97bd91aa3
Akai <nguyensieu12112002@gmail.com>
2025-05-04 23:38:24 +0700

    Create ServiceResultExtensions.cs

---
+++ Shared/PM.Shared.Dtos/ServiceResultExtensions.cs
@@ -0,0 +1,10 @@
+﻿namespace PM.Shared.Dtos
+{
+    public static class ServiceResultExtensions
+    {
+        public static bool IsSuccess<T>(this ServiceResult<T> result)
+        {
+            return result.Status == ResultStatus.Success && result.Data != null;
+        }
+    }
+}
