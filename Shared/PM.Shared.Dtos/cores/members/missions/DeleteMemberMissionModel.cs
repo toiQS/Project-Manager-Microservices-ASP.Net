4d496753ee34847d2b4e27dcf89d8b09b0cd9b22
Akai <nguyensieu12112002@gmail.com>
2025-05-05 01:15:22 +0700

    Create DeleteMemberMissionModel.cs

---
+++ Shared/PM.Shared.Dtos/cores/members/missions/DeleteMemberMissionModel.cs
@@ -0,0 +1,8 @@
+﻿namespace PM.Shared.Dtos.cores.members.missions
+{
+    public class DeleteMemberMissionModel
+    {
+        public string MemberId { get; set; } = string.Empty;
+        public string MissionId { get; set; } = string.Empty;
+    }
+}
