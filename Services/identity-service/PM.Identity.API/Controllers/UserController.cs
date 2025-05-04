8fb5735bde0d32485ca0641a704e9d9ec389f0d3
Akai <nguyensieu12112002@gmail.com>
2025-05-04 23:38:00 +0700

    Update UserController.cs

--- Services/identity-service/PM.Identity.API/Controllers/UserController.cs
+++ Services/identity-service/PM.Identity.API/Controllers/UserController.cs
@@ -1,6 +1,6 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 using PM.Identity.Application.Interfaces;
-using PM.Shared.Dtos.auths;
+using PM.Shared.Dtos;
 using PM.Shared.Dtos.tracking;
 using PM.Shared.Dtos.users;
 using System.Net.Http.Json;
