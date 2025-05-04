9223c12863feaee470fd85f6576d8a3c59c8d028
Akai <nguyensieu12112002@gmail.com>
2025-05-05 01:15:10 +0700

    Create IProjectMemberHandle.cs

---
+++ Services/core-service/PM.Core.Application/Interfaces/IProjectMemberHandle.cs
@@ -0,0 +1,19 @@
+﻿using PM.Shared.Dtos.cores.members.projects;
+using PM.Shared.Dtos;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PM.Core.Application.Interfaces
+{
+    public  interface IProjectMemberHandle
+    {
+        Task<ServiceResult<IEnumerable<IndexProjectMemberModel>>> GetMembersInProject(string projectId);
+        Task<ServiceResult<IEnumerable<IndexProjectMemberModel>>> AddAsync(string userId, AddProjectMemberModel model);
+        Task<ServiceResult<IEnumerable<IndexProjectMemberModel>>> PatchAsync(string userId, string memberId, PacthProjectMemberModel model);
+        Task<ServiceResult<IEnumerable<IndexProjectMemberModel>>> DeleteAsync(string userId, string memberId); 
+        Task<ServiceResult<IEnumerable<IndexProjectMemberModel>>> DeteleManyAsync(string userId, string projectId);
+    }
+}
