1b2c4ca3e6e5e117d73331e3d9963bad4f7bc809
Akai <nguyensieu12112002@gmail.com>
2025-05-05 01:15:27 +0700

    Create AddProjectMemberModel.cs

---
+++ Shared/PM.Shared.Dtos/cores/members/projects/AddProjectMemberModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PM.Shared.Dtos.cores.members.projects
+{
+    public class AddProjectMemberModel
+    {
+        public string PositionId { get; set; } = string.Empty;
+        public string ProjectId { get; set; } = string.Empty;
+        public string UserId {  get; set; } = string.Empty;
+    }
+}
