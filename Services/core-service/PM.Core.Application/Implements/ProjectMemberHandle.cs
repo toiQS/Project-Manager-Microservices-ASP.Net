3f641bc48db8fb6f52e6ac3d9d8a008cda6c2349
Akai <nguyensieu12112002@gmail.com>
2025-05-04 23:38:52 +0700

    Create ProjectMemberHandle.cs

---
+++ Services/core-service/PM.Core.Application/Implements/ProjectMemberHandle.cs
@@ -0,0 +1,27 @@
+﻿using PM.Core.Application.Interfaces;
+using PM.Core.Entities;
+using PM.Core.Infrastructure.Data;
+using PM.Shared.Dtos;
+using PM.Shared.Dtos.cores.members;
+using PM.Shared.Dtos.users;
+using PM.Shared.Handle.Interfaces;
+
+namespace PM.Core.Application.Implements
+{
+    public class ProjectMemberHandle
+    {
+        private readonly IUnitOfWork<CoreDbContext> _unitOfWork;
+        private readonly IAPIService<UserDetail> _userAPI;
+        private readonly IPositionHandle _positionHandle;
+        private readonly Position _ownerPosition;
+        public ProjectMemberHandle(IUnitOfWork<CoreDbContext> unitOfWork, IAPIService<UserDetail> userAPI, IPositionHandle positionHandle)
+        {
+            _unitOfWork = unitOfWork;
+            _userAPI = userAPI;
+            _positionHandle = positionHandle;
+            _ownerPosition = _positionHandle.GetPositionByName("Product Owner").GetAwaiter().GetResult().Data;
+        }
+        
+
+    }
+}
