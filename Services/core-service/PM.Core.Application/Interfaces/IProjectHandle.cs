06c9b1c7c17c877b62dfb478f13850c09432d116
Akai <nguyensieu12112002@gmail.com>
2025-05-05 10:50:53 +0700

    Update IProjectHandle.cs

--- Services/core-service/PM.Core.Application/Interfaces/IProjectHandle.cs
+++ Services/core-service/PM.Core.Application/Interfaces/IProjectHandle.cs
@@ -6,5 +6,10 @@ namespace PM.Core.Application.Interfaces
     public interface IProjectHandle
     {
         Task<ServiceResult<IEnumerable<IndexProjectModel>>> ProjectsUserHasJoined(string userId);
+        Task<ServiceResult<IEnumerable<IndexProjectModel>>> ProjectUserIsOwner(string userId);
+        Task<ServiceResult<IEnumerable<IndexProjectModel>>> GetProjectsByText(string text);
+        Task<ServiceResult<DetailProjectModel>> AddAsync(string userId, AddProjectModel model);
+        Task<ServiceResult<DetailProjectModel>> PatchAsync(string userId, string projectId, PatchProjectModel model);
+        Task<ServiceResult<IEnumerable<IndexProjectModel>>> DeleteAsync(string userId, string projectId);
     }
 }
